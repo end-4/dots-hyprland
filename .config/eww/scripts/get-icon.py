72263291af3b773883ace9fa6ec05e466654a8d4
end-4 <dnhatminh1312@gmail.com>
2023-01-19 15:50:19 +0700

    overview hehee
    a better activities list
    still cant display positions and screenshots

--- .config/eww/scripts/get-icon.py
+++ .config/eww/scripts/get-icon.py
@@ -4,6 +4,14 @@ import sys
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+# Predefine class corrections
+if sys.argv[1] == 'Code':
+    sys.argv[1] = 'code'
+if sys.argv[1] == 'GitHub Desktop':
+    sys.argv[1] = 'github-desktop'
+if sys.argv[1] == 'org.kde.kolourpaint':
+    sys.argv[1] = 'kolourpaint'
+
 icon_name = sys.argv[1]
 icon_theme = Gtk.IconTheme.get_default()
 icon = icon_theme.lookup_icon(icon_name, 48, 0)
