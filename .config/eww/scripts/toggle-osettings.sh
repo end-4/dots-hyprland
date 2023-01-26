9d7e01eb903f38f7b436699fc9288d7dc59a00a5
end-4 <dnhatminh1312@gmail.com>
2023-01-26 09:29:30 +0700

    more osettings options

--- .config/eww/scripts/toggle-osettings.sh
+++ .config/eww/scripts/toggle-osettings.sh
@@ -4,12 +4,14 @@ if [[ "$state" == "false" ]]; then
     eww open osettings 
     eww update oquery=''
     hyprctl keyword monitor eDP-1, addreserved, 32, 0, 30, -30
+    # hyprctl keyword decoration:dim_inactive true
     eww update rev_ostg=true
     sleep 0.3
     wtype -k tab
 else
     eww update rev_ostg=false
     hyprctl keyword monitor eDP-1, addreserved, 32, 0, 0, 0
+    # hyprctl keyword decoration:dim_inactive false
     sleep 0.3
     eww close osettings 
     eww update oquery=''
