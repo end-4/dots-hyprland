f4b9597e324b20c508e2167faa87f923bddf8c09
end-4 <dnhatminh1312@gmail.com>
2023-02-04 22:24:19 +0700

    osu branch as default

--- .config/eww/scripts/toggle-osettings.sh
+++ .config/eww/scripts/toggle-osettings.sh
@@ -1,16 +1,26 @@
+#!/usr/bin/bash
 state=$(eww get rev_ostg)
+state_ontf=$(eww get rev_ontf)
 
 if [[ "$state" == "false" ]]; then
+    if [[ "$state_ontf" == "true" ]]; then
+        eww update rev_ontf=false
+        eww update force_sys_rev=false
+    fi
     eww open osettings 
     eww update oquery=''
-    hyprctl keyword monitor eDP-1, addreserved, 32, 0, 30, -30
+    hyprctl keyword monitor eDP-1,addreserved,53,0,30,-30
     # hyprctl keyword decoration:dim_inactive true
     eww update rev_ostg=true
     sleep 0.3
-    wtype -k tab
+    if [[ "$state_ostg" == "true" ]]; then
+        eww close onotify 
+    fi
+    sleep 1
+    # wtype -k tab
 else
     eww update rev_ostg=false
-    hyprctl keyword monitor eDP-1, addreserved, 32, 0, 0, 0
+    hyprctl keyword monitor eDP-1,addreserved,53,0,0,0
     # hyprctl keyword decoration:dim_inactive false
     sleep 0.3
     eww close osettings 
