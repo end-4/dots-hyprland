e8830c1a4005b024b9ad8dbb8c617fd6802bb6d7
end-4 <dnhatminh1312@gmail.com>
2023-02-25 21:37:06 +0700

    gradient, theme sync

--- .config/eww/scripts/toggle-onotify.sh
+++ .config/eww/scripts/toggle-onotify.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/bash
-
-getopen(){
-  ans=$(eww windows | grep -e "$1")
-  if [ "${ans:0:1}" == "*" ]; then
-    echo "true"
-  else 
-    echo "false"
-  fi
-}
-
 state=$(eww get rev_ontf)
 state_ostg=$(eww get rev_ostg)
 
-if [[ "$state" == "false" ]]; then
-    eww open onotify
+if [[ "$state" == "true" || "$1" == "--close" ]]; then
+    eww update rev_ontf=false
+    eww update force_sys_rev=false
+    hyprctl keyword monitor eDP-1,addreserved,53,0,0,0
+    # hyprctl keyword decoration:dim_inactive false
+    sleep 0.3
+    eww close onotify 
+else
+  eww open onotify
     if [[ "$state_ostg" == "true" ]]; then
         eww update rev_ostg=false
     fi
@@ -26,11 +23,4 @@ if [[ "$state" == "false" ]]; then
         eww close osettings 
         eww update oquery=''
     fi
-else
-    eww update rev_ontf=false
-    eww update force_sys_rev=false
-    hyprctl keyword monitor eDP-1,addreserved,53,0,0,0
-    # hyprctl keyword decoration:dim_inactive false
-    sleep 0.3
-    eww close onotify 
 fi
