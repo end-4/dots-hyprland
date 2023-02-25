ea10f02275e9d3c16a208917376e610ccdc43b3a
end-4 <dnhatminh1312@gmail.com>
2023-02-25 21:37:06 +0700

    gradient, theme sync

--- .config/eww/scripts/toggle-osettings.sh
+++ .config/eww/scripts/toggle-osettings.sh
@@ -2,7 +2,14 @@
 state=$(eww get rev_ostg)
 state_ontf=$(eww get rev_ontf)
 
-if [[ "$state" == "false" ]]; then
+if [[ "$state" == "true" || "$1" == "--close" ]]; then
+    eww update rev_ostg=false
+    hyprctl keyword monitor eDP-1,addreserved,53,0,0,0
+    # hyprctl keyword decoration:dim_inactive false
+    sleep 0.3
+    eww close osettings 
+    eww update oquery=''
+else # state = false
     if [[ "$state_ontf" == "true" ]]; then
         eww update rev_ontf=false
         eww update force_sys_rev=false
@@ -16,12 +23,4 @@ if [[ "$state" == "false" ]]; then
     if [[ "$state_ostg" == "true" ]]; then
         eww close onotify 
     fi
-    sleep 1
-else
-    eww update rev_ostg=false
-    hyprctl keyword monitor eDP-1,addreserved,53,0,0,0
-    # hyprctl keyword decoration:dim_inactive false
-    sleep 0.3
-    eww close osettings 
-    eww update oquery=''
 fi
