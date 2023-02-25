886773ddbbff712a3d841439bb8b503acb3e6e0a
end-4 <dnhatminh1312@gmail.com>
2023-02-25 11:47:08 +0700

    application launcher in overview
    There's no place like Gnome.

---
+++ .config/eww/scripts/toggle-overview.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+state=$(~/.config/eww/scripts/isopen.sh overview)
+
+cd ~/.config/eww
+
+if [[ "$state" == "false" ]]; then 
+    scripts/listentrynames.py &
+    scripts/listentries.py &
+    eww update overview_query='' 
+    eww update overview_hover_name="Activities Overview"
+    eww open --toggle overview 
+else
+    eww open --toggle overview 
+    eww update overview_query='' 
+fi
