a97603b5f6817d02a38a57b6569ca47819cbf111
end-4 <dnhatminh1312@gmail.com>
2023-03-02 22:05:05 +0700

    updated video script to support discord

--- .local/bin/record-script.sh
+++ .local/bin/record-script.sh
@@ -3,9 +3,9 @@
 cd /home/end/Videos
 if ["$(pidof wf-recorder)" -ne ""]; then
     rm recording.mp4
-    notify-send "wf-recorder" "Starting recording"
-    wf-recorder --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor --geometry "$(slurp)" 
+    notify-send "wf-recorder" "Starting recording" -a 'wf-recorder'
+    wf-recorder -t --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor --geometry "$(slurp)" 
 else
     /usr/bin/kill --signal SIGINT wf-recorder
-    notify-send "wf-recorder" "Recording Stopped"
+    notify-send "wf-recorder" "Recording Stopped" -a 'wf-recorder'
 fi
