8638e88f94564b90be72578db0073dd45bb68666
end-4 <dnhatminh1312@gmail.com>
2023-03-02 22:05:05 +0700

    updated video script to support discord

--- .local/bin/record-script-fullscreen.sh
+++ .local/bin/record-script-fullscreen.sh
@@ -3,9 +3,9 @@
 cd /home/end/Videos
 if ["$(pidof wf-recorder)" -ne ""]; then
     rm recording.mp4
-    notify-send "wf-recorder" "Starting recording"
-    wf-recorder
+    notify-send "wf-recorder" "Starting recording" -a 'wf-recorder'
+    wf-recorder -t
 else
     /usr/bin/kill --signal SIGINT wf-recorder
-    notify-send "wf-recorder" "Recording Stopped"
+    notify-send "wf-recorder" "Recording Stopped" -a 'wf-recorder'
 fi
