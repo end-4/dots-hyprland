8d65d8fdcd4a9a65eee175c179ebccd8f65750c8
end-4 <dnhatminh1312@gmail.com>
2023-03-12 16:48:20 +0700

    eww sync...
    i lost track of what ive made 😔

--- .config/eww/scripts/toggle-dash.sh
+++ .config/eww/scripts/toggle-dash.sh
@@ -4,9 +4,12 @@ state=$(eww get rev_dash)
 if [[ "$state" == "true" || "$1" == "--close" ]]; then
     eww update anim_open_dash=false
     eww update rev_dash=false
-    # sleep 0.25
+    sleep 0.08
     eww close dashboard
 else
+    scripts/toggle-overview.sh --close &
+    scripts/toggle-osettings.sh --close &
+    scripts/toggle-onotify.sh --close &
     eww update anim_open_dash=true
     eww open dashboard
     # hyprctl keyword decoration:dim_inactive true
--- .config/eww/scripts/toggle-osettings.sh
+++ .config/eww/scripts/toggle-osettings.sh
@@ -3,18 +3,19 @@ state=$(eww get rev_ostg)
 state_ontf=$(eww get rev_ontf)
 
 if [[ "$state" == "true" || "$1" == "--close" ]]; then
-    eww update anim_open_ostg=false
-    eww update rev_ostg=false
-    hyprctl keyword monitor eDP-1,addreserved,53,0,0,0
+    eww update anim_open_ostg=false &
+    eww update rev_ostg=false &
+    hyprctl keyword monitor eDP-1,addreserved,53,0,0,0 &
     # hyprctl keyword decoration:dim_inactive false
     sleep 0.3
     eww close osettings 
     eww update oquery=''
 else # state = false
+    ~/.config/eww/scripts/toggle-overview.sh --close &
     eww update anim_open_ostg=true
     if [[ "$state_ontf" == "true" ]]; then
-        eww update anim_open_ontf=false
-        eww update rev_ontf=false
+        eww update anim_open_ontf=false &
+        eww update rev_ontf=false &
         eww update force_sys_rev=false &
     fi
     eww open osettings 
@@ -22,7 +23,7 @@ else # state = false
     eww update oquery=''
     hyprctl keyword monitor eDP-1,addreserved,53,0,30,-30
     # hyprctl keyword decoration:dim_inactive true
-    eww update rev_ostg=true
+    eww update rev_ostg=true &
     sleep 0.3
     if [[ "$state_ostg" == "true" ]]; then
         eww close onotify 
