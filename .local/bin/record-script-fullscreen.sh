9495e3917ce4159444e6a54e39b7f6011d63997b
end-4 <dnhatminh1312@gmail.com>
2023-04-30 11:48:01 +0700

    recording script: use dates in filename

--- .local/bin/record-script-fullscreen.sh
+++ .local/bin/record-script-fullscreen.sh
@@ -2,9 +2,8 @@
 
 cd /home/end/Videos
 if ["$(pidof wf-recorder)" -ne ""]; then
-    rm recording.mp4
     notify-send "wf-recorder" "Starting recording" -a 'wf-recorder'
-    wf-recorder -t
+    wf-recorder -t -f './recording_'"$(date '+%Y_%m_%_d..%H.%M')"'.mp4' --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor
 else
     /usr/bin/kill --signal SIGINT wf-recorder
     notify-send "wf-recorder" "Recording Stopped" -a 'wf-recorder'
