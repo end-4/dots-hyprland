19adfc583cfb9d546902107678f5f87b1996dece
end-4 <dnhatminh1312@gmail.com>
2023-05-15 15:44:23 +0700

    update main style
    (cuz kept secrets for 2 week for the ricing competition)

--- .config/eww/scripts/toggle-osettings.sh
+++ .config/eww/scripts/toggle-osettings.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/bash
+reserves=$(hyprctl monitors -j | gojq -r -c '.[0]["reserved"]')
+if [[ "$1" == "--keypress" && "$reserves" == "[0,0,0,50]" ]]; then
+    cd ~/.config/eww
+    scripts/toggle-winnews.sh
+    exit
+fi
+
+
 state=$(eww get rev_ostg)
-state_ontf=$(eww get rev_ontf)
 
 if [[ "$state" == "true" || "$1" == "--close" ]]; then
-    eww update anim_open_ostg=false &
-    eww update rev_ostg=false &
+    eww update anim_open_ostg=false
+    eww update rev_ostg=false
     sleep 0.15
     eww close osettings 
     eww update oquery=''
 else # state = false
-    eww update anim_open_ostg=true &
+    eww update anim_open_ostg=true
     eww update oquery='' &
     eww open osettings
     eww update rev_ostg=true &
