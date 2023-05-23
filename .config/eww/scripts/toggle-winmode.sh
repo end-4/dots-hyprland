f5a93c6b9e107c66abea9522c8ac621fa657420f
end-4 <97237370+end-4@users.noreply.github.com>
2023-05-23 20:40:50 +0700

    eww: windows: correct rounding & border, add wip task view

--- .config/eww/scripts/toggle-winmode.sh
+++ .config/eww/scripts/toggle-winmode.sh
@@ -7,22 +7,17 @@ if [[ "$reserves" == "[0,61,0,0]" ]]; then
     eww close bar
     hyprctl keyword monitor eDP-1,addreserved,0,50,0,0
 
-    hyprctl keyword decoration:rounding 0
-    hyprctl keyword general:gaps_in 0
-    hyprctl keyword general:gaps_out 0
-    hyprctl keyword general:border_size 0
-    hyprctl keyword windowrulev2 'rounding 15, floating:1'
+    hyprctl keyword decoration:rounding 8
+    hyprctl keyword general:border_size 1
     hyprctl keyword decoration:drop_shadow true
+
+    hyprctl keyword general:col.active_border 'rgba(494949aa)'
+    hyprctl keyword general:col.inactive_border 'rgba(494949aa)'
 else
     eww close winbar
     eww open bar
     eww open bottomline
     hyprctl keyword monitor eDP-1,addreserved,61,0,0,0
 
-    hyprctl keyword decoration:rounding 17 
-    hyprctl keyword general:gaps_in 4 
-    hyprctl keyword general:gaps_out 8
-    hyprctl keyword general:border_size 2
-    hyprctl keyword windowrulev2 'unset, floating:1'
-    hyprctl keyword decoration:drop_shadow false
+    hyprctl reload
 fi
