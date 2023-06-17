43b45b2cef51bf576cdb6db63b96d8ac72714d36
end-4 <97237370+end-4@users.noreply.github.com>
2023-06-17 15:29:01 +0700

    eww: generate color schemes into ./scripts/cache instead of ./tmp/

--- .config/eww/scripts/toggle-sideright.sh
+++ .config/eww/scripts/toggle-sideright.sh
@@ -6,8 +6,10 @@ if [[ "$state" == "true" || "$1" == "--close" ]]; then
     eww update bar_offset=0
     sleep 0.15
 else
+    cd ~/.config/eww || exit
     eww open sideright
     eww update open_sideright=true
     eww update bar_offset=-1
     eww update open_sideleft=false
+    eww update notifications="$(scripts/notifget)"
 fi
