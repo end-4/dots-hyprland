cf52f1e146baf6cb510d14444224e9f595948670
end-4 <97237370+end-4@users.noreply.github.com>
2023-06-27 17:50:10 +0700

    hyprland: monitors: bruh it was simpler than i thought

--- .config/hypr/scripts/set-resolution.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-screenwidth=$(hyprctl monitors -j | gojq -r '.[0]["width"]')
-screenheight=$(hyprctl monitors -j | gojq -r '.[0]["height"]')
-framerate=$(qalc "ceil("$(hyprctl monitors -j | gojq -r '.[0]["refreshRate"]')")" | cut -d= -f2 | tr -d ' ')
-monitor=$(hyprctl monitors -j | gojq -r '.[0]["name"]')
-
-echo hyprctl keyword monitor "$monitor","$screenwidth"x"$screenheight"@"$framerate",0x0,1
-hyprctl keyword monitor "$monitor","$screenwidth"x"$screenheight"@"$framerate",0x0,1
