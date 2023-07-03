23c9a8531b7889606516f0bbdd71aec7d7e0d3a8
end-4 <97237370+end-4@users.noreply.github.com>
2023-07-03 08:57:59 +0700

    sync july 3

--- .config/eww/scripts/toggle-dashboard.sh
+++ .config/eww/scripts/toggle-dashboard.sh
@@ -2,9 +2,9 @@
 state=$(eww get open_dashboard)
 
 if [[ "$state" -gt "0" || "$1" == "--close" ]]; then
+    eww close dashboard
     eww update open_dashboard=0
     sleep 0.1
-    eww close dashboard
     eww update cavajson=''
 else
     eww open dashboard
@@ -14,4 +14,12 @@ else
     eww update open_dashboard=2
     sleep 0.05
     eww update open_dashboard=3
+    sleep 0.05
+    eww update open_dashboard=4
+    sleep 0.05
+    eww update open_dashboard=5
+    sleep 0.05
+    eww update open_dashboard=6
+    sleep 0.05
+    eww update open_dashboard=7
 fi
