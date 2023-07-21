d9ee09584ea3a6194b5e132d7423f4cb64f69b05
end-4 <97237370+end-4@users.noreply.github.com>
2023-07-21 08:07:33 +0700

    px to rem. pain.

--- .config/eww/scripts/toggle-sideright.sh
+++ .config/eww/scripts/toggle-sideright.sh
@@ -3,10 +3,13 @@ state=$(eww get open_sideright)
 
 if [[ "$state" == "true" || "$1" == "--close" ]]; then
     eww update open_sideright=false
+    sleep 0.2
+    eww close sideright
 else
     cd ~/.config/eww || exit
     eww open sideright
     eww update open_sideright=true
     eww update open_sideleft=false
+    eww close sideleft
     eww update notifications="$(scripts/notifget)"
 fi
