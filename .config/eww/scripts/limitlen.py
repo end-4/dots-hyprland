c29de39e323bf3472472a54b5323c8d8df3455bf
Hardy <freeappsforall.softworks@protonmail.com>
2023-09-13 06:19:28 +0700

    Refactor limit_length function, add docstring

--- .config/eww/scripts/limitlen.py
+++ .config/eww/scripts/limitlen.py
@@ -1,26 +1,35 @@
 #!/bin/python3
 import sys
+import unicodedata
 
-def limit_length(s, newlength):
-    # Use len() function to get number of characters in s
+
+def limit_length(string, new_length) -> str:
+    """
+    Truncate a string to a specified length, adding '...' at the end if necessary.
+
+    :param string: The input string.
+    :param new_length: The desired maximum length of the output string.
+    :return: The truncated string.
+    """
     char_count = 0
-    newstr = ''
-    # Use unicodedata.east_asian_width() function to check for double-width characters
-    import unicodedata
-    for c in s:
+    new_string = ''
+
+    for character in string:
         char_count += 1
-        if unicodedata.east_asian_width(c) == 'W':
+        if unicodedata.east_asian_width(character) == 'W':
             char_count += 1
-        if char_count <= newlength:
-            newstr += c
+        if char_count <= new_length:
+            new_string += character
         else:
-            newstr = newstr + '...'
+            new_string += '...'
             break
-    # Add double-width count to character count to get display length
-    return newstr
 
-original = sys.argv[1]
-newlen = int(sys.argv[2])
-newstr = limit_length(original, newlen)
+    return new_string
 
-print(newstr)
+
+if __name__ == '__main__':
+    original = sys.argv[1]
+    new_len = int(sys.argv[2])
+    new_str = limit_length(original, new_len)
+
+    print(new_str)
