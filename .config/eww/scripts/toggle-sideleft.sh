340ff81418cfe215f7d312c68b08d82b1f8ce502
Karl Harrenga <70955799+Lo10Th@users.noreply.github.com>
2023-10-26 18:30:21 +0000

    fixed not open sideleft

--- .config/eww/scripts/toggle-sideleft.sh
+++ .config/eww/scripts/toggle-sideleft.sh
@@ -5,8 +5,8 @@ if [[ "$state" == "true" || "$1" == "--close" ]]; then
     eww update open_sideleft=false
     eww update bar_offset=0
 else
-    eww open sideleft
     eww update open_sideleft=true
     eww update bar_offset=1
     eww update open_sideright=false
+    eww open sideleft
 fi
--- .config/eww/scripts/toggle-sideright.sh
+++ .config/eww/scripts/toggle-sideright.sh
@@ -6,9 +6,9 @@ if [[ "$state" == "true" || "$1" == "--close" ]]; then
     eww update bar_offset=0
 else
     cd ~/.config/eww || exit
-    eww open sideright
     eww update open_sideright=true
     eww update bar_offset=-1
     eww update open_sideleft=false
     eww update notifications="$(scripts/notifget)"
+    eww open sideright
 fi
