f4bf2af70927ec54bfad82858a66708944628e98
end-4 <97237370+end-4@users.noreply.github.com>
2023-11-30 17:37:21 +0700

    use record script in ags folder

--- .config/ags/scripts/record-script.sh
+++ .config/ags/scripts/record-script.sh
@@ -1,12 +1,20 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
+getdate() {
+    date '+%Y%m%d_%H-%M-%S'
+}
 
 cd ~/Videos || exit
 if [[ "$(pidof wf-recorder)" == "" ]]; then
-    notify-send "Starting recording" './recording_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.mp4' -a 'record-script.sh'
+    notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'record-script.sh'
     if [[ "$1" == "--sound" ]]; then
-        wf-recorder -t -f './recording_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.mp4' --geometry "$(slurp)"  --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor
+        wf-recorder -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor
+    elif [[ "$1" == "--fullscreen-sound" ]]; then
+        wf-recorder -f './recording_'"$(getdate)"'.mp4' -t --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor
+    elif [[ "$1" == "--fullscreen" ]]; then
+        wf-recorder -f './recording_'"$(getdate)"'.mp4' -t
     else 
-        wf-recorder -t -f './recording_'"$(date '+%Y_%m_%_d..%H.%M.%S')"'.mp4' --geometry "$(slurp)" 
+        wf-recorder -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" 
     fi
 else
     /usr/bin/kill --signal SIGINT wf-recorder
