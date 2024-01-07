2f77f5fde200ea165dbcb6071077c78f0d1822f0
end-4 <97237370+end-4@users.noreply.github.com>
2024-01-07 16:50:43 +0700

    Update YOU_SHOULD_NOT_USE_THIS_BRANCH.sh

--- YOU_SHOULD_NOT_USE_THIS_BRANCH.sh
+++ YOU_SHOULD_NOT_USE_THIS_BRANCH.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 echo 'Use another branch, as said in the readme'
+echo 'For example, running this would switch to illogical-impulse branch:'
+echo ''
+echo 'git checkout illogical-impulse'
