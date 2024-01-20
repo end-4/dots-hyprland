8b28b2a41ee6603379343f701b434a74bd909df7
end-4 <97237370+end-4@users.noreply.github.com>
2024-01-20 14:24:06 +0700

    fix wallpaper for multimonitor (#200); recommend image size

--- .config/ags/services/wallpaper.js
+++ .config/ags/services/wallpaper.js
@@ -1,4 +1,4 @@
-const { Gio, GLib } = imports.gi;
+const { Gdk, Gio, GLib } = imports.gi;
 import Service from 'resource:///com/github/Aylur/ags/service.js';
 import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 const { exec, execAsync } = Utils;
@@ -19,6 +19,7 @@ class WallpaperService extends Service {
 
     _wallPath = '';
     _wallJson = [];
+    _monitorCount = 1;
 
     _save() {
         Utils.writeFile(JSON.stringify(this._wallJson), this._wallPath)
@@ -32,7 +33,8 @@ class WallpaperService extends Service {
     }
 
     set(path, monitor = -1) {
-        if(this._wallJson.length == 0) this._wallJson.push("");
+        this._monitorCount = Gdk.Display.get_default()?.get_n_monitors() || 1;
+        if (this._wallJson.length < this._monitorCount) this._wallJson[this._monitorCount - 1] = "";
         if (monitor == -1)
             this._wallJson.fill(path);
         else
@@ -48,17 +50,20 @@ class WallpaperService extends Service {
 
     constructor() {
         super();
+        // How many screens?
+        this._monitorCount = Gdk.Display.get_default()?.get_n_monitors() || 1;
+        // Read config
         this._wallPath = `${GLib.get_user_cache_dir()}/ags/user/wallpaper.json`;
-        if (!fileExists(this._wallPath)) { // No? create file with empty array
+        try {
+            const fileContents = Utils.readFile(this._wallPath);
+            this._wallJson = JSON.parse(fileContents);
+        }
+        catch {
             Utils.exec(`bash -c 'mkdir -p ${GLib.get_user_cache_dir()}/ags/user'`);
             Utils.exec(`touch ${this._wallPath}`);
             Utils.writeFile('[]', this._wallPath).then(() => {
                 this._wallJson = JSON.parse(Utils.readFile(this._wallPath))
             }).catch(print);
-        }
-        else {
-            const fileContents = Utils.readFile(this._wallPath);
-            this._wallJson = JSON.parse(fileContents);
         }
     }
 }
