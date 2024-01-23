141073e9e457409722af5ff05cb4f9c119bcb67c
clsty <celestial.y@outlook.com>
2024-01-23 12:42:38 +0800

    Minor fix on checkdeps.sh.

--- checkdeps.sh
+++ checkdeps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Check whether deps exists in AUR or repos of Arch.
+# Check whether pkgs exist in AUR or repos of Arch.
+# This is a workaround for https://github.com/end-4/dots-hyprland/discussions/204
 # Do NOT abuse this since it consumes extra bandwidth from AUR server.
 
 cd "$(dirname "$0")"
@@ -15,3 +16,4 @@ echo "The non-existent pkgs in $pkglistfile_orig are listed as follows."
 # Borrowed from https://bbs.archlinux.org/viewtopic.php?pid=1490795#p1490795
 comm -23 <(sort -u $pkglistfile) <(sort -u <(wget -q -O - https://aur.archlinux.org/packages.gz | gunzip) <(pacman -Ssq))
 echo "End. If nothing appears, then all pkgs exist."
+rm $pkglistfile
