a2bf63a866e003c75421180e99753cbd17d2fbf9
clsty <celestial.y@outlook.com>
2024-01-23 12:43:34 +0800

    Minor fix on checkdeps.sh.

--- checkdeps.sh
+++ checkdeps.sh
@@ -15,5 +15,5 @@ cat $pkglistfile_orig | sed "s_\ _\n_g" > $pkglistfile
 echo "The non-existent pkgs in $pkglistfile_orig are listed as follows."
 # Borrowed from https://bbs.archlinux.org/viewtopic.php?pid=1490795#p1490795
 comm -23 <(sort -u $pkglistfile) <(sort -u <(wget -q -O - https://aur.archlinux.org/packages.gz | gunzip) <(pacman -Ssq))
-echo "End. If nothing appears, then all pkgs exist."
+echo "End of list. If nothing appears, then all pkgs exist."
 rm $pkglistfile
