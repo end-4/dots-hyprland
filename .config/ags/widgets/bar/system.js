5b23394396935cc8b67cd4adf0418b180e101677
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-01 14:30:44 +0700

    weather as fallback for battery

--- .config/ags/widgets/bar/system.js
+++ .config/ags/widgets/bar/system.js
@@ -8,8 +8,11 @@ const { GLib } = imports.gi;
 import Battery from 'resource:///com/github/Aylur/ags/service/battery.js';
 import { MaterialIcon } from '../../lib/materialicon.js';
 import { AnimatedCircProg } from "../../lib/animatedcircularprogress.js";
+import { WWO_CODE, WEATHER_SYMBOL, NIGHT_WEATHER_SYMBOL } from '../../data/weather.js';
 
 const BATTERY_LOW = 20;
+const WEATHER_CACHE_FOLDER = `${GLib.get_user_cache_dir()}/ags/weather`;
+Utils.exec(`mkdir -p ${WEATHER_CACHE_FOLDER}`);
 
 const BatBatteryProgress = () => {
     const _updateProgress = (circprog) => { // Set circular progress value
@@ -90,7 +93,7 @@ const BarBattery = () => Box({
             transitionDuration: 150,
             revealChild: false,
             transition: 'slide_right',
-            child: MaterialIcon('bolt', 'norm', {tooltipText: "Charging"}),
+            child: MaterialIcon('bolt', 'norm', { tooltipText: "Charging" }),
             setup: (self) => self.hook(Battery, revealer => {
                 self.revealChild = Battery.charging;
             }),
@@ -130,6 +133,73 @@ const BarGroup = ({ child }) => Widget.Box({
         }),
     ]
 });
+const BatteryModule = () => Stack({
+    transition: 'slide_up_down',
+    transitionDuration: 150,
+    items: [
+        ['laptop', Box({
+            className: 'spacing-h-5', children: [
+                BarGroup({ child: Utilities() }),
+                BarGroup({ child: BarBattery() }),
+            ]
+        })],
+        ['desktop', BarGroup({
+            child: Box({
+                hexpand: true,
+                hpack: 'center',
+                className: 'spacing-h-5',
+                children: [
+                    MaterialIcon('device_thermostat', 'small'),
+                    Label({
+                        label: 'Weather',
+                    })
+                ],
+                setup: (self) => self.poll(900000, async (self) => {
+                    const WEATHER_CACHE_PATH = WEATHER_CACHE_FOLDER + '/wttr.in.txt';
+                    Utils.execAsync('curl ipinfo.io')
+                        .then(output => {
+                            return JSON.parse(output)['city'].toLowerCase();
+                        })
+                        .then((city) => execAsync(`curl https://wttr.in/${city}?format=j1`)
+                            .then(output => {
+                                console.log(`curl https://wttr.in/${city}?format=j1`);
+                                const weather = JSON.parse(output);
+                                Utils.writeFile(JSON.stringify(weather), WEATHER_CACHE_PATH)
+                                    .catch(print);
+                                const weatherCode = weather.current_condition[0].weatherCode;
+                                const weatherDesc = weather.current_condition[0].weatherDesc[0].value;
+                                const temperature = weather.current_condition[0].temp_C;
+                                const feelsLike = weather.current_condition[0].FeelsLikeC;
+                                const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
+                                self.children[0].label = weatherSymbol;
+                                self.children[1].label = `${temperature}℃ • Feels like ${feelsLike}℃`;
+                                self.tooltipText = weatherDesc;
+                            }).catch((err) => {
+                                try { // Read from cache
+                                    const weather = JSON.parse(
+                                        Utils.readFile(WEATHER_CACHE_PATH)
+                                    );
+                                    const weatherCode = weather.current_condition[0].weatherCode;
+                                    const weatherDesc = weather.current_condition[0].weatherDesc[0].value;
+                                    const temperature = weather.current_condition[0].temp_C;
+                                    const feelsLike = weather.current_condition[0].FeelsLikeC;
+                                    const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
+                                    self.children[0].label = weatherSymbol;
+                                    self.children[1].label = `${temperature}℃ • Feels like ${feelsLike}℃`;
+                                    self.tooltipText = weatherDesc;
+                                } catch (err) {
+                                    print(err);
+                                }
+                            }));
+                }),
+            })
+        })],
+    ],
+    setup: (stack) => Utils.timeout(10, () => {
+        if (!Battery.available) stack.shown = 'desktop';
+        else stack.shown = 'laptop';
+    })
+})
 
 const switchToRelativeWorkspace = async (self, num) => {
     try {
@@ -140,6 +210,7 @@ const switchToRelativeWorkspace = async (self, num) => {
     }
 }
 
+
 export default () => Widget.EventBox({
     onScrollUp: (self) => switchToRelativeWorkspace(self, -1),
     onScrollDown: (self) => switchToRelativeWorkspace(self, +1),
@@ -148,30 +219,7 @@ export default () => Widget.EventBox({
         className: 'spacing-h-5',
         children: [
             BarGroup({ child: BarClock() }),
-            Stack({
-                transition: 'slide_up_down',
-                transitionDuration: 150,
-                items: [
-                    ['laptop', Box({
-                        className: 'spacing-h-5', children: [
-                            BarGroup({ child: Utilities() }),
-                            BarGroup({ child: BarBattery() }),
-                        ]
-                    })],
-                    ['desktop', Box({
-                        className: 'spacing-h-5',
-                        children: [
-                            Label({
-                                label: 'Weather',
-                            })
-                        ]
-                    })],
-                ],
-                setup: (stack) => Utils.timeout(10, () => {
-                    if (!Battery.available) stack.shown = 'desktop';
-                    else stack.shown = 'laptop';
-                })
-            })
+            BatteryModule(),
         ]
     })
 });
