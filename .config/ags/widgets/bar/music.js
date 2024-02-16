0afc5aa03904065690bcf832bae07454a38da7b0
clsty <celestial.y@outlook.com>
2024-02-17 07:11:41 +0800

    Fix locale for CPU/RAM/SWAP on bar.

--- .config/ags/widgets/bar/music.js
+++ .config/ags/widgets/bar/music.js
@@ -145,7 +145,7 @@ export default () => {
     const systemResources = BarGroup({
         child: Box({
             children: [
-                BarResource('RAM Usage', 'memory', `free | awk '/^Mem/ {printf("%.2f\\n", ($3/$2) * 100)}'`),
+                BarResource('RAM Usage', 'memory', `LANG=C free | awk '/^Mem/ {printf("%.2f\\n", ($3/$2) * 100)}'`),
                 Revealer({
                     revealChild: true,
                     transition: 'slide_left',
@@ -153,8 +153,8 @@ export default () => {
                     child: Box({
                         className: 'spacing-h-10 margin-left-10',
                         children: [
-                            BarResource('Swap Usage', 'swap_horiz', `free | awk '/^Swap/ {if ($2 > 0) printf("%.2f\\n", ($3/$2) * 100); else print "0";}'`),
-                            BarResource('CPU Usage', 'settings_motion_mode', `top -bn1 | grep Cpu | sed 's/\\,/\\./g' | awk '{print $2}'`),
+                            BarResource('Swap Usage', 'swap_horiz', `LANG=C free | awk '/^Swap/ {if ($2 > 0) printf("%.2f\\n", ($3/$2) * 100); else print "0";}'`),
+                            BarResource('CPU Usage', 'settings_motion_mode', `LANG=C top -bn1 | grep Cpu | sed 's/\\,/\\./g' | awk '{print $2}'`),
                         ]
                     }),
                     setup: (self) => self.hook(Mpris, label => {
