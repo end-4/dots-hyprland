b024e4b7112d1aea3e9ee2c43ea96ef84f82b716
clsty <celestial.y@outlook.com>
2024-02-16 22:09:01 +0800

    Notification about using main branch.

--- .config/ags/scripts/color_generation/switchcolor.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-color=$(hyprpicker --no-fancy)
-
-# Generate colors for ags n stuff
-"$HOME"/.config/ags/scripts/color_generation/colorgen.sh "${color}" --apply
--- .config/ags/scripts/hyprland/workspace_action.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-hyprctl dispatch "$1" $(((($(hyprctl activeworkspace -j | gojq -r .id) - 1)  / 10) * 10 + $2))
