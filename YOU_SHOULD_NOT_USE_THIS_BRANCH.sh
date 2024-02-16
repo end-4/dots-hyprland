20382ff1eb491ff20e491917e70625ba770aec04
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-16 17:33:31 +0700

    Delete YOU_SHOULD_NOT_USE_THIS_BRANCH.sh

--- YOU_SHOULD_NOT_USE_THIS_BRANCH.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo 'Use another branch, as said in the readme'
-echo 'For example, running this would switch to illogical-impulse branch:'
-echo ''
-echo 'git checkout illogical-impulse'
