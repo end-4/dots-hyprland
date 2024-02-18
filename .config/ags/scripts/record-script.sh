fd0c95d2c87b7b00cfe4f0c296bbb376caa54e7c
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-18 22:08:03 +0700

    fix record script for non-nvidia

--- .config/ags/scripts/record-script.sh
+++ .config/ags/scripts/record-script.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-## hmm
-# --pixel-format yuv420p 
-
 getdate() {
     date '+%Y%m%d_%H-%M-%S'
 }
@@ -17,13 +14,13 @@ if pgrep wf-recorder > /dev/null; then
 else
     notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'record-script.sh'
     if [[ "$1" == "--sound" ]]; then
-        wf-recorder -c h264_vaapi -d /dev/dri/renderD128 -C aac -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" --audio="$(getaudiooutput)" & disown
+        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" --audio="$(getaudiooutput)" & disown
     elif [[ "$1" == "--fullscreen-sound" ]]; then
-        wf-recorder -c h264_vaapi -d /dev/dri/renderD128 -C aac -f './recording_'"$(getdate)"'.mp4' -t --audio="$(getaudiooutput)" & disown
+        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --audio="$(getaudiooutput)" & disown
     elif [[ "$1" == "--fullscreen" ]]; then
-        wf-recorder -c h264_vaapi -d /dev/dri/renderD128 -C aac -f './recording_'"$(getdate)"'.mp4' -t & disown
+        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t & disown
     else 
-        wf-recorder -c h264_vaapi -d /dev/dri/renderD128 -C aac -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" & disown
+        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" & disown
     fi
 fi
 
