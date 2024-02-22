3b4ced65916e4461f11b224693d1adf803d60eba
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-22 20:28:05 +0700

    no more swaylock (#278)

--- uninstall.sh
+++ uninstall.sh
@@ -20,7 +20,7 @@ set -e
 # Undo Step 3: Removing copied config and local folders
 printf '\e[36mRemoving copied config and local folders...\n\e[97m'
 
-for i in ags fish fontconfig foot fuzzel hypr mpv swaylock wlogout "starship.toml" rubyshot
+for i in ags fish fontconfig foot fuzzel hypr mpv wlogout "starship.toml" rubyshot
   do v rm -rf "$HOME/.config/$i"
 done
 
@@ -45,6 +45,6 @@ v sudo deluser "$user" input
 read -p "Do you want to uninstall packages used by the dotfiles?\nCtrl+C to exit, or press Enter to proceed"
 
 # Removing installed yay packages and dependencies
-v yay -Rns adw-gtk3-git brightnessctl cava foot fuzzel gjs gojq gradience-git grim gtk-layer-shell hyprland-git lexend-fonts-git libdbusmenu-gtk3 plasma-browser-integration playerctl python-build python-material-color-utilities python-poetry python-pywal ripgrep sassc swww slurp starship swayidle swaylock tesseract ttf-jetbrains-mono-nerd ttf-material-symbols-variable-git ttf-space-mono-nerd typescript webp-pixbuf-loader wl-clipboard wlogout yad ydotool
+v yay -Rns adw-gtk3-git brightnessctl cava foot fuzzel gjs gojq gradience-git grim gtk-layer-shell hyprland-git lexend-fonts-git libdbusmenu-gtk3 plasma-browser-integration playerctl python-build python-material-color-utilities python-poetry python-pywal ripgrep sassc swww slurp starship swayidle hyprlock-git tesseract ttf-jetbrains-mono-nerd ttf-material-symbols-variable-git ttf-space-mono-nerd typescript webp-pixbuf-loader wl-clipboard wlogout yad ydotool
 
 printf '\e[36mUninstall Complete.\n\e[97m'
