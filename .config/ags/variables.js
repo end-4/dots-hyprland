a93399b1502dea1bb21bee0f48285b8c9264625b
end-4 <97237370+end-4@users.noreply.github.com>
2024-02-22 16:20:13 +0700

    fix music controls keybind

--- .config/ags/variables.js
+++ .config/ags/variables.js
@@ -1,13 +1,16 @@
 import Variable from 'resource:///com/github/Aylur/ags/variable.js';
+import Mpris from 'resource:///com/github/Aylur/ags/service/mpris.js';
 const { exec } = Utils;
-
+import Gdk from 'gi://Gdk';
 
 // Global vars for external control (through keybinds)
 export const showMusicControls = Variable(false, {})
 export const showColorScheme = Variable(false, {})
 globalThis['openMusicControls'] = showMusicControls;
 globalThis['openColorScheme'] = showColorScheme;
+globalThis['mpris'] = Mpris;
 
 // Screen size
 export const SCREEN_WIDTH = Number(exec(`bash -c "xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1 | head -1" | awk '{print $1}'`));
 export const SCREEN_HEIGHT = Number(exec(`bash -c "xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2 | head -1" | awk '{print $1}'`));
+
