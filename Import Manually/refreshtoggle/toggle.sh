ffe2894faea502b966897019941ea62b43d11c7a
harshafaik <kyouyata405@gmail.com>
2024-03-07 16:46:43 +0000

    assigning a variable to HRR.

--- Import Manually/refreshtoggle/toggle.sh
+++ Import Manually/refreshtoggle/toggle.sh
@@ -1,7 +1,8 @@
-# Set the default values for the monitor, resolution, and refresh rate
+# Set the default values for the monitor, resolution, and refresh rates.
 monitor="eDP-1"
 resolution="1920x1080"
 refresh_rate="60"
+high_refresh_rate="144"
 
 while true; do
     charger_status=$(acpi -a | cut -d' ' -f3 | cut -d- -f1)
@@ -11,7 +12,7 @@ while true; do
     fi
 
     if [ "$charger_status" == "on" ]; then
-        hyprctl keyword monitor $monitor, ${resolution}@144,0x0,1
+        hyprctl keyword monitor $monitor, ${resolution}@${high_refresh_rate},0x0,1
     fi
 
     sleep 5
