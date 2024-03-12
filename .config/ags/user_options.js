32956fda25ac1d7bb71735013e86acc504121c89
end-4 <97237370+end-4@users.noreply.github.com>
2024-03-12 14:43:30 +0700

    allow user config (related: #271) (closes #318)

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -1,82 +1,9 @@
 
-let userConfigOptions = {
-    // General stuff
-    'ai': {
-        'defaultGPTProvider': "openai",
-        'defaultTemperature': 0.9,
-        'writingCursor': " ...", // Warning: Using weird characters can mess up Markdown rendering
-    },
-    'animations': {
-        'durationSmall': 110,
-        'durationLarge': 180,
-    },
-    'apps': {
-        'imageViewer': "loupe",
-        'terminal': "foot", // This is only for shell actions
-    },
-    'battery': {
-        'low': 20,
-        'critical': 10,
-    },
-    'music': {
-        'preferredPlayer': "plasma-browser-integration",
-    },
-    'onScreenKeyboard': {
-        'layout': "qwerty_full", // See modules/onscreenkeyboard/onscreenkeyboard.js for available layouts
-    },
-    'overview': {
-        'scale': 0.18, // Relative to screen size
-        'numOfRows': 2,
-        'numOfCols': 5,
-        'wsNumScale': 0.09,
-        'wsNumMarginScale': 0.07,
-    },
-    'sidebar': {
-        'imageColumns': 2,
-    },
-    'search': {
-        'engineBaseUrl': "https://www.google.com/search?q=",
-        'excludedSites': ["quora.com"],
-    },
-    'time': {
-        // See https://docs.gtk.org/glib/method.DateTime.format.html
-        // Here's the 12h format: "%I:%M%P"
-        // For seconds, add "%S" and set interval to 1000
-        'format': "%H:%M",
-        'interval': 5000,
-        'dateFormatLong': "%A, %d/%m", // On bar
-        'dateInterval': 5000,
-        'dateFormat': "%d/%m", // On notif time
-    },
-    'weather': {
-        'city': "",
-    },
-    'workspaces': {
-        'shown': 10,
-    },
-    // Longer stuff
-    'icons': {
-        substitutions: {
-            'code-url-handler': "visual-studio-code",
-            'Code': "visual-studio-code",
-            'GitHub Desktop': "github-desktop",
-            'Minecraft* 1.20.1': "minecraft",
-            'gnome-tweaks': "org.gnome.tweaks",
-            'pavucontrol-qt': "pavucontrol",
-            'wps': "wps-office2019-kprometheus",
-            'wpsoffice': "wps-office2019-kprometheus",
-            '': "image-missing",
-        }
-    },
+const userConfigOptions = {
+    // For every option, see ~/.config/ags/modules/.configuration/user_options.js (vscode users ctrl+click this file://./modules/.configuration/user_options.js)
+    // Options listed in this file will override the default ones in the above file
+    // Here's an example
     'keybinds': { 
-        // Format: Mod1+Mod2+key. CaSe SeNsItIvE!
-        // Modifiers: Shift Ctrl Alt Hyper Meta
-        // See https://docs.gtk.org/gdk3/index.html#constants for the other keys (they are listed as KEY_key)
-        'overview': {
-            'altMoveLeft': "Ctrl+b",
-            'altMoveRight': "Ctrl+f",
-            'deleteToEnd': "Ctrl+k",
-        },
         'sidebar': {
             'apis': {
                 'nextTab': "Page_Down",
@@ -90,5 +17,4 @@ let userConfigOptions = {
     },
 }
 
-globalThis['userOptions'] = userConfigOptions;
-export default userOptions;
+export default userConfigOptions;
