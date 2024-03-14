8afbe57aad4eb068750bfe2f3d4a23f095a319b0
clsty <celestial.y@outlook.com>
2024-03-14 22:50:07 +0800

    Tip for (neo)vim users editing user_options.js

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -1,6 +1,8 @@
 
 const userConfigOptions = {
-    // For every option, see ~/.config/ags/modules/.configuration/user_options.js (vscode users ctrl+click this file://./modules/.configuration/user_options.js)
+    // For every option, see ~/.config/ags/modules/.configuration/user_options.js
+    // (tip4vscode: `ctrl+click` this: file://./modules/.configuration/user_options.js)
+    // (tip4vim: `:vsp` to split window, move cursor to this path, press `gf`. `Ctrl-w` twice to switch between)
     // Options listed in this file will override the default ones in the above file
     // Here's an example
     'keybinds': {
