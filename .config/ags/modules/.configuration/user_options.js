f3878f9ddd94dccc932cf3af1adafbb2195c1fab
jsl9208 <shilong@heywhale.com>
2024-03-16 05:09:09 +0800

    add device and minPercent brightness config

--- .config/ags/modules/.configuration/user_options.js
+++ .config/ags/modules/.configuration/user_options.js
@@ -21,6 +21,10 @@ let configOptions = {
         'low': 20,
         'critical': 10,
     },
+    'brightness': {
+      'device': '', // `-d` param passed to brightnessctl, leave empty to use brightnessctl default
+      'minPercent': 0, // Lowest brightness (0-100)
+    },
     'music': {
         'preferredPlayer': "plasma-browser-integration",
     },
