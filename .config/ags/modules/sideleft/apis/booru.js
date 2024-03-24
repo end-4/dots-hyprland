0edf57a6c1fa1989350eb5b501174b31eaa6610f
end-4 <97237370+end-4@users.noreply.github.com>
2024-03-24 09:09:19 +0700

    focus entry on opening sideleft

--- .config/ags/modules/sideleft/apis/booru.js
+++ .config/ags/modules/sideleft/apis/booru.js
@@ -348,7 +348,7 @@ export const booruView = Scrollable({
         // Always scroll to bottom with new content
         const adjustment = scrolledWindow.get_vadjustment();
         adjustment.connect("changed", () => {
-            if(!chatEntry.hasFocus) return;
+            if (!chatEntry.hasFocus) return;
             adjustment.set_value(adjustment.get_upper() - adjustment.get_page_size());
         })
     }
