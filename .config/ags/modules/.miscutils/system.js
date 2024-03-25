7d7f2e923bf57b0cc7528635bf6a8498bedd2da6
end-4 <97237370+end-4@users.noreply.github.com>
2024-03-25 13:01:30 +0700

    fedora system icon

--- .config/ags/modules/.miscutils/system.js
+++ .config/ags/modules/.miscutils/system.js
@@ -20,6 +20,8 @@ export const getDistroIcon = () => {
     if(distroID == 'cachyos') return 'cachyos-symbolic';
     // Funny flake
     if(distroID == 'nixos') return 'nixos-symbolic';
+    // Cool thing
+    if(distroID == 'fedora') return 'fedora-symbolic';
     // Debians
     if(distroID == 'linuxmint') return 'ubuntu-symbolic';
     if(distroID == 'ubuntu') return 'ubuntu-symbolic';
@@ -38,6 +40,8 @@ export const getDistroName = () => {
     if(distroID == 'cachyos') return 'CachyOS';
     // Funny flake
     if(distroID == 'nixos') return 'NixOS';
+    // Cool thing
+    if(distroID == 'fedora') return 'Fedora';
     // Debians
     if(distroID == 'linuxmint') return 'Linux Mint';
     if(distroID == 'ubuntu') return 'Ubuntu';
