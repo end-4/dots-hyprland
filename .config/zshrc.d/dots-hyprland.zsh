b0fc49f3922dc60bbf3edc952030cd1f1fffae5c
clsty <celestial.y@outlook.com>
2024-03-31 06:21:48 +0800

    Add optional autostart on tty scripts

--- .config/zshrc.d/dots-hyprland.zsh
+++ .config/zshrc.d/dots-hyprland.zsh
@@ -1,7 +1,4 @@
-# Created by newuser for 5.9
-# Shortcuts
-bindkey '^H' backward-kill-word 
-bindkey '^Z' undo
+# Use the generated color scheme
 
 if test -f ~/.cache/ags/user/generated/terminal/sequences.txt; then
     cat ~/.cache/ags/user/generated/terminal/sequences.txt
---
+++ .config/zshrc.d/shortcuts.zsh
@@ -0,0 +1,4 @@
+# Created by newuser for 5.9
+
+bindkey '^H' backward-kill-word 
+bindkey '^Z' undo
