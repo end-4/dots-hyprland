484d9bd940638afd1643e2c2b4946366733b5f1c
end-4 <97237370+end-4@users.noreply.github.com>
2024-04-03 22:11:08 +0700

    config option for disabling smoke; remove debug print

--- .config/ags/modules/.configuration/user_options.js
+++ .config/ags/modules/.configuration/user_options.js
@@ -18,6 +18,7 @@ let configOptions = {
     },
     'appearance': {
         'keyboardUseFlag': false, // Use flag emoji instead of abbreviation letters
+        'layerSmoke': true,
     },
     'apps': {
         'bluetooth': "blueberry",
