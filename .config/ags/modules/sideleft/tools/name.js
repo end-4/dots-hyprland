d3e44e1c09030458e0d2535ba1291da8a87969dd
end-4 <97237370+end-4@users.noreply.github.com>
2024-04-19 10:54:21 +0700

    sidebar: add github link to make tools page less empty

---
+++ .config/ags/modules/sideleft/tools/name.js
@@ -0,0 +1,26 @@
+const { Gtk } = imports.gi;
+import App from 'resource:///com/github/Aylur/ags/app.js';
+import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
+import { setupCursorHover } from '../../.widgetutils/cursorhover.js';
+const { execAsync, exec } = Utils;
+const { Box, Button, CenterBox, EventBox, Icon, Label, Scrollable } = Widget;
+
+export default () => Box({
+    className: 'txt sidebar-module techfont',
+    children: [
+        Label({
+            label: 'illogical-impulse'
+        }),
+        Box({ hexpand: true }),
+        Button({
+            className: 'sidebar-module-btn-arrow',
+            onClicked: () => execAsync(['xdg-open', 'https://github.com/end-4/dots-hyprland']).catch(print),
+            child: Icon({
+                className: 'txt txt-norm',
+                icon: 'github-symbolic',
+            }),
+            setup: setupCursorHover,
+        })
+    ]
+})
