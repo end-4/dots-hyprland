5662cb04e9b1bd6fca9af9a28ce15a9789f1f376
end-4 <97237370+end-4@users.noreply.github.com>
2024-04-29 22:30:36 +0700

    sideright: update centermodules buttons

--- .config/ags/modules/sideright/centermodules/notificationlist.js
+++ .config/ags/modules/sideright/centermodules/notificationlist.js
@@ -67,9 +67,10 @@ export default (props) => {
         ,
     });
     const ListActionButton = (icon, name, action) => Button({
-        className: 'notif-listaction-btn',
+        className: 'sidebar-centermodules-bottombar-button',
         onClicked: action,
         child: Box({
+            hpack: 'center',
             className: 'spacing-h-5',
             children: [
                 MaterialIcon(icon, 'norm'),
