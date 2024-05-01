40d7ceaae9e030a66cc7910c0c744557e8805cf2
end-4 <97237370+end-4@users.noreply.github.com>
2024-05-01 21:39:41 +0700

    remove unnecessary comments

--- .config/ags/scripts/color_generation/colorgen.sh
+++ .config/ags/scripts/color_generation/colorgen.sh
@@ -11,8 +11,6 @@ lightdark="dark"
 transparency="opaque"
 materialscheme="vibrant"
 terminalscheme="$HOME/.config/ags/scripts/templates/terminal/scheme-base.json"
-# terminalscheme="$HOME/.config/ags/scripts/templates/terminal/scheme-catppuccin.json"
-# terminalscheme="$HOME/.config/ags/scripts/templates/terminal/scheme-vscode.json"
 
 if [ ! -f $colormodefile ]; then
     echo "dark" > $colormodefile
