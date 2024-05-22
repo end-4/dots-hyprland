4aa9e3d916898d74407dd8478cf8d0cdfa78e29e
end-4 <97237370+end-4@users.noreply.github.com>
2024-05-22 17:59:13 +0700

    uninst: delete state and  config folders (#525)

--- uninstall.sh
+++ uninstall.sh
@@ -10,8 +10,9 @@ function v() {
 printf 'Hi there!\n'
 printf 'This script 1. will uninstall [end-4/dots-hyprland > illogical-impulse] dotfiles\n'
 printf '            2. will try to revert *mostly everything* installed using install.sh, so it'\''s pretty destructive\n'
-printf '            3. has not beed tested, use at your own risk.\n'
-printf '            4. will show all commands that it runs.\n'
+printf '            3. has not been tested, use at your own risk.\n'
+printf '            4. assumes you have default xdg dirs (for example config folder in ~/.config).\n'
+printf '            5. will show all commands that it runs.\n'
 printf 'Ctrl+C to exit. Enter to continue.\n'
 read -r
 set -e
@@ -25,6 +26,8 @@ for i in ags fish fontconfig foot fuzzel hypr mpv wlogout "starship.toml" rubysh
 done
 
 v rm -rf "$HOME/.local/bin/fuzzel-emoji"
+v rm -rf "$HOME/.cache/ags"
+v sudo rm -rf "$HOME/.local/state/ags"
 
 ##############################################################################################################################
 
