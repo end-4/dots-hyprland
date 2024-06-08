875238430655af6e8ef1a78c1ba8ef376edc1ecb
end-4 <97237370+end-4@users.noreply.github.com>
2024-06-08 20:20:50 +0700

    bar: add "nothing" mode (#544)

--- .config/ags/modules/bar/main.js
+++ .config/ags/modules/bar/main.js
@@ -77,11 +77,14 @@ export const Bar = async (monitor = 0) => {
         endWidget: Widget.Box({}),
         setup: (self) => {
             self.hook(Battery, (self) => {
-                if(!Battery.available) return;
+                if (!Battery.available) return;
                 self.toggleClassName('bar-bg-focus-batterylow', Battery.percent <= userOptions.battery.low);
             })
         }
     });
+    const nothingContent = Widget.Box({
+        className: 'bar-bg-nothing',
+    })
     return Widget.Window({
         monitor,
         name: `bar${monitor}`,
@@ -95,9 +98,11 @@ export const Bar = async (monitor = 0) => {
             children: {
                 'normal': normalBarContent,
                 'focus': focusedBarContent,
+                'nothing': nothingContent,
             },
             setup: (self) => self.hook(currentShellMode, (self) => {
                 self.shown = currentShellMode.value;
+
             })
         }),
     });
