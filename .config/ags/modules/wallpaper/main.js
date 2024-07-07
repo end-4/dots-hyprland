a0e029de8da9c642ddcc4780e96c2cd805697c1e
Myryk <myryk@posteo.de>
2024-07-07 20:45:16 +0200

    removing unnecessary stuff

--- .config/ags/modules/wallpaper/main.js
+++ .config/ags/modules/wallpaper/main.js
@@ -1,60 +1,12 @@
 import PopupWindow from '../.widgethacks/popupwindow.js';
 import * as Utils from 'resource:///com/github/Aylur/ags/utils.js'
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-const { Box, Window } = Widget;
 
-//const b = Variable(false);
 const dir = userOptions.wallpaper.path;
 const scriptDir = `${App.configDir}/scripts/color_generation/switchwall.sh`;
 
-/*
-function updateFiles() {
-    files.value = Utils.exec(`find ${dir} -type f | grep -E '.jpg$|.jpeg$|.png$|.gif$'`);
-}
-
-*/
-//function OpenWallpaper() {
-    //    return Widget.Button({
-//        child: Widget.Icon({ icon: "starred" }),
-//        onClicked: () => {
-    //            updateFiles()
-    //            b.value = !b.value
-//            App.toggleWindow('wallpaper')
-//        }
-//    })
-//}
-
-// const PopupWindow = ({
-//     name,
-//     child,
-//     showClassName = "",
-//     hideClassName = "",
-//     ...props
-// }) => {
-//     return Window({
-//         name,
-//         visible: false,
-//         layer: 'overlay',
-//         ...props,
-
-//         // child: child,
-//         child: Box({
-//             setup: (self) => {
-//                 self.keybind("Escape", () => closeEverything());
-//                 if (showClassName != "" && hideClassName !== "") {
-//                     self.hook(App, (self, currentName, visible) => {
-//                         if (currentName === name) {
-//                             self.toggleClassName(hideClassName, !visible);
-//                         }
-//                     });
-
-//                     if (showClassName !== "" && hideClassName !== "")
-//                         self.className = `${showClassName} ${hideClassName}`;
-//                 }
-//             },
-//             child: child,
-//         }),
-//     });
+// function updateFiles() {
+//     files.value = Utils.exec(`find ${dir} -type f | grep -E '.jpg$|.jpeg$|.png$|.gif$'`);
 // }
 
 export const WallpaperPicker = (id) => {
@@ -64,7 +16,6 @@ export const WallpaperPicker = (id) => {
         class_name: "wallpaper",
         monitor: id,
         anchor: ["top", "left", "right"],
-        // anchor: ["left", "top", "bottom"],
         layer: "overlay",
         keymode: "on-demand",
         margins: [7],
@@ -78,19 +29,10 @@ export const WallpaperPicker = (id) => {
                 children: files.split("\n").map(path => ImagesList(path, id))
             }),
         }),
-        /* setup: (self) => {
-            self.keybind("Escape", () => closeEverything());
-        }, */
     })
 }
 
 function ImagesList(path, id) {
-    let gif = path.endsWith(".gif");
-    let thumbnail = `${path.substr(path.lastIndexOf(".") + 1, path.length)}`;
-    // thumbnail = thumbnail.substr(0, thumbnail.lastIndexOf(".") - 1);
-    // console.log(thumbnail);
-    // thumbnail += `\x00icon\x1f${path}`;
-    // console.log(thumbnail);
     return Widget.Button({
         class_name: 'wallpaperButton',
         onPrimaryClick: () => {
