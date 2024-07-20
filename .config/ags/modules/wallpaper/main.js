8a958bda27cbc5c44d13e66b97fa2bcfa364eca8
Myryk <myryk@posteo.de>
2024-07-20 19:24:34 +0200

    clickcloseregion added,

--- .config/ags/modules/wallpaper/main.js
+++ .config/ags/modules/wallpaper/main.js
@@ -1,6 +1,7 @@
 import PopupWindow from '../.widgethacks/popupwindow.js';
 import * as Utils from 'resource:///com/github/Aylur/ags/utils.js'
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import clickCloseRegion from '../.commonwidgets/clickcloseregion.js';
 const { Gio, GLib, Gtk, GdkPixbuf, Gdk } = imports.gi;
 
 const dir = userOptions.wallpaper.path;
@@ -10,26 +11,36 @@ const scriptDir = `${App.configDir}/scripts/color_generation/switchwall.sh`;
 //     files.value = Utils.exec(`find ${dir} -type f | grep -E '.jpg$|.jpeg$|.png$|.gif$'`);
 // }
 
-export const WallpaperPicker = (id) => {
-    let files = Utils.exec(`bash -c "find ${dir} -type f | grep -E '.jpg$|.jpeg$|.png$ '"`);
-    // console.log(Object.keys(Gdk));
-    return PopupWindow({
-        name: `wallpaperpicker${id}`,
-        class_name: "wallpaper",
-        monitor: id,
-        anchor: ["top", "left", "right"],
-        layer: "overlay",
-        keymode: "on-demand",
-        margins: [7],
+const wallpaperScrollable = (id, files) => {
+    return Widget.Box({
+        class_name: 'wallpaperContainer',
         child: Widget.Scrollable({
             class_name: 'wallpaperScroll',
             hexpand: true,
             hscroll: "always",
             vscroll: "never",
             child: Widget.Box({
-                class_name: 'wallpaperContainer',
                 children: files.split("\n").map(path => ImagesList(path, id))
             }),
+        }),
+    });
+};
+
+export const WallpaperPicker = (id) => {
+    let files = Utils.exec(`bash -c "find ${dir} -type f | grep -E '.jpg$|.jpeg$|.png$ '"`);
+    // console.log(Object.keys(Gdk));
+    return PopupWindow({
+        name: `wallpaperpicker${id}`,
+        monitor: id,
+        anchor: ["top", "left", "right"],
+        layer: "overlay",
+        keymode: "on-demand",
+        child: Widget.Box({
+            vertical: true,
+            children: [
+                wallpaperScrollable(id, files),
+                clickCloseRegion({ name: `wallpaperpicker` , fillMonitor: `vertical`}),
+            ],
         }),
     })
 }
@@ -68,7 +79,7 @@ function ImagesList(path, id) {
                     App.closeWindow(`wallpaperpicker${id}`);
                     setWallpaper(path);
                 },
-                // setup : (self) => {
+                // setup: (self) => {
                 //     let test = self;
                 //     Utils.idle((test) => {
                 //         if (basename.lastIndexOf(".") + 1 == "gif") {
@@ -107,7 +118,7 @@ export function autoWallpaper() {
 function setWallpaper(path) {
     let smartflag = userOptions.wallpaper.smart ? '--smart' : '';
     let popupflag = userOptions.wallpaper.popup ? '' : '--no-popup';
-    console.log(`sh ${scriptDir} ${path} ${smartflag} ${popupflag}`);
+    // console.log(`sh ${scriptDir} ${path} ${smartflag} ${popupflag}`);
     Utils.execAsync(['bash', '-c', `sh "${scriptDir}" "${path}" "${smartflag}" "${popupflag}"`]).catch(print);
 }
 
