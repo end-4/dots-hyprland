a80c03b592f51af2fc41ee0ebfc97000f678aed6
end-4 <97237370+end-4@users.noreply.github.com>
2024-08-19 10:55:03 +0700

    fix fake screen rounding + add bar rounding config (#701)

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -53,11 +53,13 @@ const Windows = () => [
     ...(userOptions.appearance.fakeScreenRounding !== 0 ? [
         forMonitors((id) => Corner(id, 'top left', true)),
         forMonitors((id) => Corner(id, 'top right', true)),
+        forMonitors((id) => Corner(id, 'bottom left', true)),
+        forMonitors((id) => Corner(id, 'bottom right', true)),
     ] : []),
-    forMonitors((id) => Corner(id, 'bottom left', userOptions.appearance.fakeScreenRounding !== 0)),
-    forMonitors((id) => Corner(id, 'bottom right', userOptions.appearance.fakeScreenRounding !== 0)),
-    forMonitors(BarCornerTopleft),
-    forMonitors(BarCornerTopright),
+    ...(userOptions.appearance.barRoundCorners ? [
+        forMonitors(BarCornerTopleft),
+        forMonitors(BarCornerTopright),
+    ] : []),
 ];
 
 const CLOSE_ANIM_TIME = 210; // Longer than actual anim time to make sure widgets animate fully
