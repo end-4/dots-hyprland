a5cfa635e986e1cf7355458f1c7132b187b35307
clsty <celestial.y@outlook.com>
2025-02-07 07:45:44 +0800

    Fix gradience-cli not using uv venv

--- .config/ags/scripts/color_generation/applycolor.sh
+++ .config/ags/scripts/color_generation/applycolor.sh
@@ -149,7 +149,9 @@ apply_gtk() { # Using gradience-cli
   done
 
   mkdir -p "$XDG_CONFIG_HOME/presets" # create gradience presets folder
-  $(eval echo $ILLOGICAL_IMPULSE_VIRTUAL_ENV)/bin/gradience-cli apply -p "$CACHE_DIR"/user/generated/gradience/preset.json --gtk both
+  source $(eval echo $ILLOGICAL_IMPULSE_VIRTUAL_ENV)/bin/activate
+  gradience-cli apply -p "$CACHE_DIR"/user/generated/gradience/preset.json --gtk both
+  deactivate
 
   # And set GTK theme manually as Gradience defaults to light adw-gtk3
   # (which is unreadable when broken when you use dark mode)
