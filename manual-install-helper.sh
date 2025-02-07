af9e89589e5f1daed8319788c24b21a27e6e7f30
clsty <celestial.y@outlook.com>
2025-02-07 12:23:11 +0800

    Minor update manual-install-helper.sh

--- manual-install-helper.sh
+++ manual-install-helper.sh
@@ -18,3 +18,7 @@ install-bibata
 install-MicroTeX
 install-uv
 install-python-packages
+
+if [[ -z "${ILLOGICAL_IMPULSE_VIRTUAL_ENV}" ]]; then
+  printf "\n\e[31m[$0]: \!! Important \!! : Please ensure environment variable \e[0m \$ILLOGICAL_IMPULSE_VIRTUAL_ENV \e[31m is set to proper value (by default \"~/.local/state/ags/.venv\"), or AGS config will not work. We have already provided this configuration in ~/.config/hypr/hyprland/env.conf, but you need to ensure it is included in hyprland.conf, and also a restart is needed for applying it.\e[0m\n"
+fi
