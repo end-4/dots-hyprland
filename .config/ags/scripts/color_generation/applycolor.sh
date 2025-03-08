c1407866f5d84a6e1251d1bb649e09022ff28b3e
Satoxyan <ayangsatya321@gmail.com>
2025-03-08 17:55:46 +0700

    Changing execution

--- .config/ags/scripts/color_generation/applycolor.sh
+++ .config/ags/scripts/color_generation/applycolor.sh
@@ -189,5 +189,4 @@ apply_fuzzel &
 apply_term &
 
 #Executing lockcolor.sh on exit
-trap '$HOME/.config/ags/scripts/color_generation/lockcolor.sh' EXIT
-trap '$HOME/.config/ags/scripts/color_generation/cavacolor.sh' EXIT
+trap '$HOME/.config/ags/scripts/color_generation/lockcolor.sh && $HOME/.config/ags/scripts/color_generation/cavacolor.sh' EXIT
