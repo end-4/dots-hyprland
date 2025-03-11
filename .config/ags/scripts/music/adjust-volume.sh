d866332a1bf634e7fa095c1eba7f0a301e65f3ad
endotrizine <53936918+endotrizine@users.noreply.github.com>
2025-03-11 14:45:35 +0500

    Update adjust-volume.sh

--- .config/ags/scripts/music/adjust-volume.sh
+++ .config/ags/scripts/music/adjust-volume.sh
@@ -2,14 +2,23 @@
 
 change=$1
 
-current_volume=$(playerctl volume)
+players=$(playerctl -l)
 
-new_volume=$(echo "$current_volume + $change" | bc)
+for player in $players; do
+    if playerctl -p "$player" volume &>/dev/null; then
+        current_volume=$(playerctl -p "$player" volume)
 
-if (( $(echo "$new_volume > 1.0" | bc -l) )); then
-  new_volume=1.0
-elif (( $(echo "$new_volume < 0.0" | bc -l) )); then
-  new_volume=0.0
-fi
+        new_volume=$(echo "$current_volume + $change" | bc)
+        
+        if (( $(echo "$new_volume > 1.0" | bc -l) )); then
+            new_volume=1.0
+        elif (( $(echo "$new_volume < 0.0" | bc -l) )); then
+            new_volume=0.0
+        fi
 
-playerctl volume "$new_volume"
+        playerctl -p "$player" volume "$new_volume"
+        exit 0
+    fi
+done
+
+exit 1
