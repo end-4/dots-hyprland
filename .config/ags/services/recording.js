19c5389634f1f87e6b249cdf3d1f15f442aeb1cf
Chiron <135429057+Chiron8@users.noreply.github.com>
2025-03-14 19:56:36 +0000

    Make recording.js more efficient

--- .config/ags/services/recording.js
+++ .config/ags/services/recording.js
@@ -3,56 +3,27 @@ import Utils from "resource:///com/github/Aylur/ags/utils.js";
 
 class RecordingService extends Service {
   static {
-    Service.register(
-      this,
-      { "recording-status": ["boolean"] },
-      { recording: "boolean" },
-    );
+    Service.register(this, { "recording-status": ["boolean"] }, { recording: "boolean" });
   }
 
   #recording = false;
-  #checkInterval = null;
 
   constructor() {
     super();
-    this.#initRecordingCheck();
-  }
-
-  #initRecordingCheck() {
-    if (this.#checkInterval) {
-      clearInterval(this.#checkInterval);
-    }
-
-    this.#checkInterval = setInterval(() => {
-      this.#checkRecordingStatus();
-    }, 2000);
+    this.#checkRecordingStatus();
+    this.interval = Utils.interval(3000, this.#checkRecordingStatus.bind(this));
   }
 
   #checkRecordingStatus() {
-    try {
-      const processCheck = Utils.exec("pgrep -x wf-recorder");
-      const newStatus = processCheck.length > 0;
-
-      if (newStatus !== this.#recording) {
-        this.#recording = newStatus;
-        this.changed("recording");
-      }
-    } catch {
-      if (this.#recording) {
-        this.#recording = false;
-        this.changed("recording");
-      }
+    const isRunning = Utils.exec("pgrep -x wf-recorder").trim() !== "";
+    if (isRunning !== this.#recording) {
+      this.#recording = isRunning;
+      this.changed("recording");
     }
   }
 
   get recording() {
     return this.#recording;
-  }
-
-  destroy() {
-    if (this.#checkInterval) {
-      clearInterval(this.#checkInterval);
-    }
   }
 }
 
