4d6ad8d92850d592db237e0bd60a6fd70d4d4442
end-4 <97237370+end-4@users.noreply.github.com>
2025-04-06 16:14:56 +0200

    remove debug flag from generate_colors_material.py

--- .config/ags/scripts/color_generation/generate_colors_material.py
+++ .config/ags/scripts/color_generation/generate_colors_material.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S\_/bin/sh\_-xc\_"source\_\$(eval\_echo\_\$ILLOGICAL_IMPULSE_VIRTUAL_ENV)/bin/activate&&exec\_python\_-E\_"\$0"\_"\$@""
+#!/usr/bin/env -S\_/bin/sh\_-c\_"source\_\$(eval\_echo\_\$ILLOGICAL_IMPULSE_VIRTUAL_ENV)/bin/activate&&exec\_python\_-E\_"\$0"\_"\$@""
 import argparse
 import math
 import json
