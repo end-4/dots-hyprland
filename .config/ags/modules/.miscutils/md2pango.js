1b9717702b9e461f530f7af8e9ef80a3f0f0a9ae
end-4 <97237370+end-4@users.noreply.github.com>
2025-04-06 15:13:36 +0200

    sidebar chat: don't process inline latex in dollar signs
    problematic with inline shell expressions

--- .config/ags/modules/.miscutils/md2pango.js
+++ .config/ags/modules/.miscutils/md2pango.js
@@ -49,8 +49,8 @@ const replaceCategory = (text, replaces) => {
 // Main function
 
 export function replaceInlineLatexWithCodeBlocks(text) {
-    return text.replace(/\\\[(.*?)\\\]|\\\((.*?)\\\)|\$(.*?)\$/gs, (match, square, round, dollar) => {
-        const latex = square || round || dollar;
+    return text.replace(/\\\[(.*?)\\\]|\\\((.*?)\\\)/gs, (match, square, round) => {
+        const latex = square || round;
         return `\n\`\`\`latex\n${latex}\n\`\`\`\n`;
     });
 }
