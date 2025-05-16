a78b56c450bf24ce2f90b79e61231e520ea5cc2e
Greyfeather <lordelden@gmail.com>
2025-05-16 14:48:29 -0600

    add jsdoc to file_utils.js

--- .config/quickshell/modules/common/functions/file_utils.js
+++ .config/quickshell/modules/common/functions/file_utils.js
@@ -1,3 +1,8 @@
+/**
+ * Trims fthe File protocol off the input string
+ * @param {string} str
+ * @returns {string}
+ */
 function trimFileProtocol(str) {
     return str.startsWith("file://") ? str.slice(7) : str;
 }
