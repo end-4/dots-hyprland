5a38388c622cf9b4099e35a367ca2e6afd85e034
end-4 <97237370+end-4@users.noreply.github.com>
2025-05-30 22:48:12 +0200

    fix install script trying to install non-exsistent packages

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ install-local-pkgbuild() {
 }
 
 # Install core dependencies from the meta-packages
-metapkgs=(./arch-packages/illogical-impulse-{audio,python,backlight,basic,fonts-themes,gnome,gtk,portal,screencapture,widgets})
+metapkgs=(./arch-packages/illogical-impulse-{audio,backlight,basic,fonts-themes,kde,portal,python,screencapture,toolkit,widgets})
 metapkgs+=(./arch-packages/illogical-impulse-agsv1)
 metapkgs+=(./arch-packages/illogical-impulse-hyprland)
 metapkgs+=(./arch-packages/illogical-impulse-microtex-git)
