9af8fc137fb034aad8dbb09ea9abad2260ab74ff
end-4 <97237370+end-4@users.noreply.github.com>
2025-05-31 16:26:08 +0200

    icons: breeze -> breeze-plus

--- install.sh
+++ install.sh
@@ -103,7 +103,7 @@ install-local-pkgbuild() {
 metapkgs=(./arch-packages/illogical-impulse-{audio,backlight,basic,fonts-themes,kde,portal,python,screencapture,toolkit,widgets})
 metapkgs+=(./arch-packages/illogical-impulse-hyprland)
 metapkgs+=(./arch-packages/illogical-impulse-microtex-git)
-metapkgs+=(./arch-packages/illogical-impulse-oneui4-icons-git)
+# metapkgs+=(./arch-packages/illogical-impulse-oneui4-icons-git)
 [[ -f /usr/share/icons/Bibata-Modern-Classic/index.theme ]] || \
   metapkgs+=(./arch-packages/illogical-impulse-bibata-modern-classic-bin)
 
