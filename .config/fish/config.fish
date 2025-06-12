bc8cb7bbe6f5e9968490e79c5e11389dcf4d0256
Jx <103049321+JxJxxJxJ@users.noreply.github.com>
2025-06-12 18:28:39 -0300

    Fix fish trailing newline before prompt
    This fixes it, not sure if something like this would be welcomed though

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,8 +13,16 @@ if status is-interactive
 end
 
 starship init fish | source
+
+# INFO: Version 1 (commented out to avoid printing unwanted newline before prompt)
+# if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+#     cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+# end
+
+# INFO: Version 2 (send terminal sequences without trailing newline before prompt)
 if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    # Avoid printing trailing newline so prompt starts at the first line
+    head -c -1 ~/.local/state/quickshell/user/generated/terminal/sequences.txt >/dev/tty
 end
 
 alias pamcan pacman
