ad73ca018a08d69612e216ee5daa5f6657cedeab
end-4 <97237370+end-4@users.noreply.github.com>
2025-06-27 22:07:57 +0200

    record script: fix fullscreen

--- .config/hypr/hyprland/scripts/record.sh
+++ .config/hypr/hyprland/scripts/record.sh
@@ -21,19 +21,22 @@ if pgrep wf-recorder > /dev/null; then
     notify-send "Recording Stopped" "Stopped" -a 'Recorder' &
     pkill wf-recorder &
 else
-    if ! region="$(slurp 2>&1)"; then
-      notify-send "Recording cancelled" "Selection was cancelled" -a 'Recorder'
-      exit 1
-    fi
-
-    notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'Recorder'
-    if [[ "$1" == "--sound" ]]; then
-        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$region" --audio="$(getaudiooutput)" & disown
-    elif [[ "$1" == "--fullscreen-sound" ]]; then
+    if [[ "$1" == "--fullscreen-sound" ]]; then
+        notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'Recorder'
         wf-recorder -o $(getactivemonitor) --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --audio="$(getaudiooutput)" & disown
     elif [[ "$1" == "--fullscreen" ]]; then
+        notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'Recorder'
         wf-recorder -o $(getactivemonitor) --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t & disown
     else
-        wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$region" & disown
+        if ! region="$(slurp 2>&1)"; then
+            notify-send "Recording cancelled" "Selection was cancelled" -a 'Recorder'
+            exit 1
+        fi
+        notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'Recorder'
+        if [[ "$1" == "--sound" ]]; then
+            wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$region" --audio="$(getaudiooutput)" & disown
+        else
+            wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$region" & disown
+        fi
     fi
 fi
