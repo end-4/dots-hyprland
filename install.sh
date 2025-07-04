c7519f338e0b169b0591616dc3cb615e2e5f472b
end-4 <97237370+end-4@users.noreply.github.com>
2025-07-04 16:58:44 +0200

    add logo (closes #1436)

--- install.sh
+++ install.sh
@@ -233,6 +233,7 @@ esac
 # some foldes (eg. .local/bin) should be processed separately to avoid `--delete' for rsync,
 # since the files here come from different places, not only about one program.
 # v rsync -av ".local/bin/" "$XDG_BIN_HOME" # No longer needed since scripts are no longer in ~/.local/bin
+v rsync -av ".local/share/icons/" "${XDG_DATA_HOME:-$HOME/.local/share}"/icons/
 
 # Prevent hyprland from not fully loaded
 sleep 1
