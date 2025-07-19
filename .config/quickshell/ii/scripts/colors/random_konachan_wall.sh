084508db844b5467e5ce76451b31599bfb986380
Dignity <107360076+77Dignity@users.noreply.github.com>
2025-07-19 19:24:49 +0200

    use different filename if already used by config

--- .config/quickshell/ii/scripts/colors/random_konachan_wall.sh
+++ .config/quickshell/ii/scripts/colors/random_konachan_wall.sh
@@ -14,11 +14,11 @@ page=$((1 + RANDOM % 1000));
 response=$(curl "https://konachan.net/post.json?tags=rating%3Asafe&limit=1&page=$page")
 link=$(echo "$response" | jq '.[0].file_url' -r); 
 ext=$(echo "$link" | awk -F. '{print $NF}')
-downloadPath="$HOME/Pictures/Wallpapers/konachan_random_image-0.$ext"
-counter=0
-while [ -e $downloadPath ]; do
-    counter=$((counter + 1))
-    downloadPath="$HOME/Pictures/Wallpapers/konachan_random_image-$counter.$ext"
-done
+downloadPath="$HOME/Pictures/Wallpapers/konachan_random_image.$ext"
+illogicalImpulseConfigPath="$HOME/.config/illogical-impulse/config.json"
+currentWallpaperPath=$(jq -r '.background.wallpaperPath' $illogicalImpulseConfigPath)
+if [ "$downloadPath" == "$currentWallpaperPath" ]; then
+    downloadPath="$HOME/Pictures/Wallpapers/konachan_random_image-1.$ext"
+fi
 curl "$link" -o "$downloadPath"
 "$SCRIPT_DIR/switchwall.sh" --image "$downloadPath"
