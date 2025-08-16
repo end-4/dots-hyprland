11d5b6cd8d266ef08ab8a8a03529fe0fc56e3252
ashmartin1810 <ashmartin1810@gmail.com>
2025-08-16 14:54:42 +0530

    Minor fix
    Minor fix, adding +$ in regex matching to disallow inputs such as "9abc". along with a --help tag. and a else condition which echos "Invalid target."

--- .config/hypr/hyprland/scripts/workspace_action.sh
+++ .config/hypr/hyprland/scripts/workspace_action.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-curr_workspace="$(hyprctl monitors -j | jq -r ".[].activeWorkspace.id")" ##parses json output of hyprctl monitors
-dispatcher="$1"; shift ##Any dispatcher that hyprland supports, the shift the arguments such that $2 is not $1
-if [[ "$1" == *"+"* || "$1" == *"-"* ]]; then ##pattern matching
+curr_workspace="$(hyprctl activeworkspace -j | jq -r ".id")" ##parses json output of hyprctl activeworkspace on the active monitor
+dispatcher="$1"
+shift ##Any dispatcher that hyprland supports, the shift shifts the target such that target is now in $1, not $2
+
+if [[ -z "${dispatcher}" || "${dispatcher}" == "--help" || "${dispatcher}" == "-h" || -z "$1" ]]; then
+  echo "Usage: $0 <dispatcher> <target>"
+fi
+if [[ "$1" == *"+"* || "$1" == *"-"* ]]; then ##pattern matching (works with r+1 and +1 only aswell)
   hyprctl dispatch "${dispatcher}" "$1" ##$1 = workspace id since we shifted earlier.
-elif [[ "$1" =~ ^[0-9] ]]; then ##Regex matching
+elif [[ "$1" =~ ^[0-9]+$ ]]; then ##Regex matching
   target_workspace=$(((($curr_workspace - 1) / 10 ) * 10 + $1)) ##decreases curr_workspace by 1, then floor division by 10. then multiplica
                                                                 ##ion by 10, and then adding $1. for eg, if $curr_workspace=24, and $1 = 6,
                                                                 ##((24-1)/10)*10 + 6
@@ -11,4 +16,6 @@ elif [[ "$1" =~ ^[0-9] ]]; then ##Regex matching
                                                                 ##2*10+6
                                                                 ##26
   hyprctl dispatch "${dispatcher}" "${target_workspace}"
+else
+ echo "Invalid target."
 fi
