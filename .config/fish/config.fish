9515317ccc5a98fae595efe05e566f5efdd82beb
Vlodimir <66350035+VLOD-ZDOV@users.noreply.github.com>
2025-08-17 15:12:52 +0700

    Update config.fish
    !! and !$

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,6 +22,33 @@ alias ls 'eza --icons'
 alias clear "printf '\033[2J\033[3J\033[1;1H'"
 alias q 'qs -c ii'
     
+# for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
 
 # function fish_prompt
 #   set_color cyan; echo (pwd)
