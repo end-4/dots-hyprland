24a1f60b8acab3a9dbe9e79a79eede68d8f73afe
whydakk <s4bba7@gmail.com>
2025-09-11 20:02:56 +0200

    Other apps init.

--- .config/fontconfig/conf.d
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-<fontconfig>
-    <match target="font">
-        <edit name="rgba" mode="assign">
-        <const>none</const>
-    </edit>
-  </match>
-</fontconfig>
