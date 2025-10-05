eb2f786c10e5349381be2febe634020e690e1fe1
Version33 <vee@versionthirtythr.ee>
2025-10-04 20:35:35 -0400

    sh

--- dist-nix/illogical-impulse-portal.nix
+++ dist-nix/illogical-impulse-portal.nix
@@ -9,10 +9,10 @@ with lib;
 
   config = mkIf config.illogical-impulse.portal.enable {
     home.packages = [
-      pkgs.kdePackages.xdg-desktop-portal
+      pkgs.xdg-desktop-portal
       pkgs.kdePackages.xdg-desktop-portal-kde
-      pkgs.kdePackages.xdg-desktop-portal-gtk
-      pkgs.kdePackages.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
     ];
   };
 }
