94749c1c69b8c64601b8aae668276d88f8cbd2d7
clsty <celestial.y@outlook.com>
2025-10-19 09:35:46 +0800

    Update comment

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 # TODO: Handle TAB completion for ./setup with non-intrusive method
-# TODO: Remove this script after TAB completion of ./setup is handled
-STY_RED='\e[31m'
-STY_RST='\e[00m'
+# TODO: Remove this script (install.sh) after TAB completion of ./setup is handled
+
+
+#STY_RED='\e[31m'
+#STY_RST='\e[00m'
 #printf "${STY_RED}You are using \"./install.sh\" which is kept for compatibility and will be removed in future.\n"
 #printf "Please use \"./setup install\" instead.${STY_RST}\n"
+
+
 cd "$(dirname "$0")"
 ./setup install "$@"
