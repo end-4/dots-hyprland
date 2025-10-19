9c216036ecd87f930f15454409b50a0bde7966f0
clsty <celestial.y@outlook.com>
2025-10-19 09:33:53 +0800

    Update message

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# TODO: Remove this script after 2025.12.01
+# TODO: Handle TAB completion for ./setup with non-intrusive method
+# TODO: Remove this script after TAB completion of ./setup is handled
 STY_RED='\e[31m'
 STY_RST='\e[00m'
-printf "${STY_RED}You are using \"./install.sh\" which is kept for compatibility and will be removed in future.\n"
-printf "Please use \"./setup install\" instead.${STY_RST}\n"
+#printf "${STY_RED}You are using \"./install.sh\" which is kept for compatibility and will be removed in future.\n"
+#printf "Please use \"./setup install\" instead.${STY_RST}\n"
 cd "$(dirname "$0")"
 ./setup install "$@"
