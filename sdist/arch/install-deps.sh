e335babb230e4f14027d68c2639122bc1e60e6f8
clsty <celestial.y@outlook.com>
2025-10-19 14:28:53 +0800

    Pin commit for quickshell-git (#2238)

--- sdist/arch/install-deps.sh
+++ sdist/arch/install-deps.sh
@@ -16,7 +16,7 @@ install-yay(){
 # However, let's just keep it as references for other distros writing their `sdist/<DISTRO_ID>/install-deps.sh`, if they need it.
 handle-deprecated-dependencies(){
   printf "${STY_CYAN}[$0]: Removing deprecated dependencies:${STY_RST}\n"
-  for i in illogical-impulse-{microtex,pymyc-aur,ags,agsv1} {hyprutils,hyprpicker,hyprlang,hypridle,hyprland-qt-support,hyprland-qtutils,hyprlock,xdg-desktop-portal-hyprland,hyprcursor,hyprwayland-scanner,hyprland}-git;do try sudo pacman --noconfirm -Rdd $i;done
+  for i in illogical-impulse-{microtex,pymyc-aur} {quickshell,hyprutils,hyprpicker,hyprlang,hypridle,hyprland-qt-support,hyprland-qtutils,hyprlock,xdg-desktop-portal-hyprland,hyprcursor,hyprwayland-scanner,hyprland}-git;do try sudo pacman --noconfirm -Rdd $i;done
 # Convert old dependencies to non explicit dependencies so that they can be orphaned if not in meta packages
   remove_bashcomments_emptylines ./sdist/arch/previous_dependencies.conf ./cache/old_deps_stripped.conf
   readarray -t old_deps_list < ./cache/old_deps_stripped.conf
@@ -77,6 +77,7 @@ install-local-pkgbuild() {
 metapkgs=(./sdist/arch/illogical-impulse-{audio,backlight,basic,fonts-themes,kde,portal,python,screencapture,toolkit,widgets})
 metapkgs+=(./sdist/arch/illogical-impulse-hyprland)
 metapkgs+=(./sdist/arch/illogical-impulse-microtex-git)
+metapkgs+=(./sdist/arch/illogical-impulse-quickshell-git)
 # metapkgs+=(./sdist/arch/packages/illogical-impulse-oneui4-icons-git)
 [[ -f /usr/share/icons/Bibata-Modern-Classic/index.theme ]] || \
   metapkgs+=(./sdist/arch/illogical-impulse-bibata-modern-classic-bin)
