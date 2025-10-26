839ca74bf1b392e2f77c854ed6ed0f8c36195a98
z0 <aervxa@gmail.com>
2025-10-26 22:00:35 +0300

    Prepend Noto Sans Arabic in fontconfig

--- dots/.config/fontconfig/conf.d
+++ dots/.config/fontconfig/conf.d
@@ -1,9 +1,14 @@
 <?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
 <fontconfig>
-    <match target="font">
-        <edit name="rgba" mode="assign">
-        <const>none</const>
+  <match target="font">
+    <edit name="rgba" mode="assign">
+      <const>none</const>
+    </edit>
+  </match>
+  <match target="pattern">
+    <edit name="family" mode="prepend" binding="strong">
+      <string>Noto Sans Arabic</string>
     </edit>
   </match>
 </fontconfig>
