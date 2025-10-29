e1a8ba09f11a97d067eb5c503cf2bbf587012841
clsty <celestial.y@outlook.com>
2025-10-29 17:31:01 +0800

    Move arabic fontconfig into dots-extra/fontsets/ar

--- dots/.config/fontconfig/conf.d
+++ dots/.config/fontconfig/conf.d
@@ -1,19 +1,9 @@
 <?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
 <fontconfig>
-  <match target="font">
-    <edit name="rgba" mode="assign">
-      <const>none</const>
-    </edit>
-  </match>
-
-  <!-- Fix for: arabic fonts rendering in Noto Nastaliq Urdu | affects Chromium, Discord (maybe all chromium based apps, but not Spotify somehow) -->
-  <match target="pattern">
-    <test compare="eq" name="family">
-      <string>sans-serif</string>
-    </test>
-    <edit name="family" mode="prepend" binding="strong">
-      <string>Noto Sans Arabic</string>
+    <match target="font">
+        <edit name="rgba" mode="assign">
+        <const>none</const>
     </edit>
   </match>
 </fontconfig>
