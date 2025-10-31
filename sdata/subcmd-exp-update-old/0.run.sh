d36966e2d703d2817f9690fff0bccd324a4a002d
clsty <celestial.y@outlook.com>
2025-10-31 09:11:03 +0800

    Still deprecate install.sh and update.sh anyway; Tidier structure

--- sdata/subcmd-exp-update-old/0.run.sh
+++ sdata/subcmd-exp-update-old/0.run.sh
@@ -477,7 +477,7 @@ if [[ "${SKIP_NOTICE}" == false ]]; then
   log_warning "THIS SCRIPT IS NOT FULLY TESTED AND MAY CAUSE ISSUES!"
   log_warning "It might be safer if you want to preserve your modifications and not delete added files,"
   log_warning "  but this can cause partial updates and therefore unexpected behavior like in #1856."
-  log_warning "In general, prefer install.sh for updates."
+  log_warning "In general, prefer ./setup install for updates."
   safe_read "Continue? (y/N): " response "N"
 
   if [[ ! "$response" =~ ^[Yy]$ ]]; then
