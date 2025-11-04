b4b15a2808ebbbacf3bb6dc8e0550aaa4395397d
clsty <celestial.y@outlook.com>
2025-11-04 10:22:28 +0800

    Update home.nix

--- sdata/dist-nix/home-manager/home.nix
+++ sdata/dist-nix/home-manager/home.nix
@@ -30,9 +30,7 @@
       ## inetutils: provides hostname, ifconfig, ping, etc.
       ## libnotify: provides notify-send
       inetutils libnotify
-
-      ##### Fonts/Icons/Cursors/Decoration #####
-      fontconfig
+      foot # Used in Quickshell and Hyprland config; its config is also included
 
       ##### Other basic things #####
       dbus xorg.xlsclients networkmanager
@@ -74,64 +72,65 @@
 
 
       ### illogical-impulse-bibata-modern-classic-bin
-      bibata-cursors #https://github.com/ful1e5/Bibata_Cursor
+      bibata-cursors #https://github.com/ful1e5/Bibata_Cursor # Used in Hyprland config, not necessary
 
 
       ### illogical-impulse-fonts-themes
-      #adw-gtk-theme-git
-      #breeze
-      #breeze-plus
-      #darkly-bin
+      adw-gtk3 #adw-gtk-theme-git (https://github.com/lassekongo83/adw-gtk3) (Used in Quickshell config)
+      kdePackages.breeze kdePackages.breeze-icons #breeze (Used in kdeglobals config)
+      #breeze-plus (https://github.com/mjkim0727/breeze-plus) (TODO: Not available as nixpkg) (Used in kde-material-you-colors config)
+      #darkly-bin (TODO: seems not being used)
       eza #eza (Used in Fish config: `alias ls 'eza --icons'`)
       #fish (Probably should not install via Nix)
       fontconfig #fontconfig (Basic thing)
       kitty #kitty (Used in fuzzel, Hyprland, kdeglobals and Quickshell config; kitty config is also included as dots)
       matugen #matugen-bin (Used in Quickshell)
-      #otf-space-grotesk https://events.ccc.de/congress/2024/infos/styleguide.html (TODO: Not available as Nixpkg) (Used in Quickshell and matugen config)
+      #otf-space-grotesk (https://events.ccc.de/congress/2024/infos/styleguide.html) (TODO: Not available as Nixpkg) (Used in Quickshell and matugen config)
       starship #starship (Used in Fish config)
-      #ttf-gabarito-git
-      #ttf-jetbrains-mono-nerd
-      #ttf-material-symbols-variable-git
-      #ttf-readex-pro
-      #ttf-roboto-flex
-      #ttf-rubik-vf
-      #ttf-twemoji
+      #ttf-gabarito-git (Font name: Gabarito) (Used in fuzzel and Quickshell config) (TODO: Not available as Nixpkg)
+      nerd-fonts.jetbrains-mono #ttf-jetbrains-mono-nerd (Font name: JetBrains Mono NF, JetBrainsMono Nerd Font) (Used in foot, kdeglobals, kitty, qt5ct, qt6ct and Quickshell config)
+      material-symbols #ttf-material-symbols-variable-git (Font name: Material Symbols Rounded, Material Symbols Outlined) (Used in Hyprland, matugen, Quickshell and wlogout config)
+      #ttf-readex-pro (Font name: Readex Pro) (Used in Quickshell config) (TODO: seems not available as nixpkg)
+      roboto-flex #ttf-roboto-flex (Font name: Roboto Flex) (Used in Hyprland, matugen and Quickshell config)
+      rubik #ttf-rubik-vf (Font name: Rubik, Rubik Light) (Used in Hyprland, kdeglobals, matugen, qt5ct, qt6ct and Quickshell config)
+      twemoji-color-font #ttf-twemoji (Not explicitly used, but it may help as fallback for displaying emoji charaters)
 
 
       ### illogical-impulse-hyprland
-      #hypridle
-      #hyprcursor
+      hypridle #hypridle (Used for loginctl to lock session)
+      #hyprcursor (TODO: Seems not being used)
       #hyprland (Need NixGL, included elsewhere)
-      #hyprland-qtutils
-      #hyprland-qt-support
-      #hyprlang
-      #hyprlock
-      #hyprpicker
-      #hyprsunset
-      #hyprutils
-      #hyprwayland-scanner
-      #xdg-desktop-portal-hyprland
-      wl-clipboard #wl-clipboard
+      #hyprland-qtutils (TODO: Not needed, it's already a dependency of Hyprland itself, and it's not being used anywhere in this repo)
+      #hyprland-qt-support (TODO: Not needed, it's already a dep of hyprland-qtutils which is a dep of Hyprland, and it's not being used anywhere in this repo)
+      #hyprlang (TODO: Not needed, it's already a dependency of Hyprland, hypridle, etc.)
+      #hyprlock (Should not be installed via Nix)
+      hyprpicker #hyprpicker (Used in Hyprland and Quickshell config)
+      hyprsunset #hyprsunset (Used in Quickshell config)
+      #hyprutils #hyprutils (TODO: Not needed, it's already a dep of Hyprland and not being used anywhere in this repo)
+      #hyprwayland-scanner (TODO: Not needed, it's already a dep of Hyprland and not being used anywhere in this repo)
+      #xdg-desktop-portal-hyprland (DUPLICATE)
+      wl-clipboard #wl-clipboard (Surely needed)
 
 
       ### illogical-impulse-kde
-      #bluedevil
-      #gnome-keyring
-      #networkmanager
-      #plasma-nm
-      #polkit-kde-agent
-      #dolphin
-      #systemsettings
+      kdePackages.bluedevil #bluedevil (Seems not being used anywhere, maybe a part of KDE settings panel)
+      gnome-keyring #gnome-keyring (TODO: Maybe should not be installed by Nix) (Provide executable gnome-keyring-daemon, used in Hyprland and Quickshell config)
+      networkmanager #networkmanager
+      kdePackages.plasma-nm #plasma-nm (Seems not being used anywhere, maybe a part of KDE settings panel)
+      #polkit-kde-agent (TODO: Maybe should not install by Nix)
+      kdePackages.dolphin #dolphin (Used in Hyprland and Quickshell config)
+      kdePackages.systemsettings #systemsettings (Used in Hyprland keybinds.conf)
 
       
       ### illogical-impulse-microtex-git
-      # This package will be installed as /opt/MicroTeX and it
+      # This package will be installed as /opt/MicroTeX
       #MicroTeX#https://github.com/NanoMichael/MicroTeX
       # TODO: Not available as nixpkg
 
 
       ### illogical-impulse-oneui4-icons-git
       #OneUI4-Icons#https://github.com/end-4/OneUI4-Icons
+      # TODO: Custom repo, need to make a package
 
 
       ### illogical-impulse-portal
@@ -142,60 +141,62 @@
 
 
       ### illogical-impulse-python
-      #clang (Some python package may need this to be built, e.g. #1235) (However it seems not available directly as a package on search.nixos.org)
+      clang #clang (Some python package may need this to be built, e.g. #1235)
       uv #uv (Used for python venv)
       gtk4 #gtk4 (Not explicitly used)
       libadwaita #libadwaita (Not explicitly used)
       libsoup_3 #libsoup3 (Not explicitly used)
       libportal-gtk4 #libportal-gtk4 (Not explicitly used)
       gobject-introspection #gobject-introspection (Not explicitly used)
-      #sassc (TODO: Not used anymore?)
+      #sassc (TODO: Not used anymore)
 
 
       ### illogical-impulse-quickshell-git
+      # TODO: maybe Quickshell uses GPU acceleration, which means NixGL is applicable
       quickshell.packages.x86_64-linux.default
 
 
       ### illogical-impulse-screencapture
-      #hyprshot
-      #slurp
-      #swappy
-      #tesseract
-      #tesseract-data-eng
-      #wf-recorder
+      hyprshot #hyprshot (Used in Hyprland keybinds.conf as fallback)
+      slurp #slurp (Used in Hyprland and Quickshell config)
+      swappy #swappy (Used in Quickshell config)
+      tesseract #tesseract (Used in Quickshell and Hyprland config)
+      #tesseract-data-eng (Used as data for tesseract) (TODO: Seems not available as nixpkg)
+      wf-recorder #wf-recorder (Used in Quickshell config)
 
 
       ### illogical-impulse-toolkit
-      #kdialog
-      #qt6-5compat 
-      #qt6-avif-image-plugin
-      #qt6-base
-      #qt6-declarative
-      #qt6-imageformats
-      #qt6-multimedia
-      #qt6-positioning
-      #qt6-quicktimeline
-      #qt6-sensors
-      #qt6-svg
-      #qt6-tools
-      #qt6-translations
-      #qt6-virtualkeyboard
-      #qt6-wayland
-      #syntax-highlighting
-      #upower
-      #wtype
-      #ydotool
+      kdePackages.kdialog #kdialog (Used in Quickshell config)
+      # TODO: Deal with qt6 things, see https://nixos.wiki/wiki/Qt
+      #qt6-5compat (Maybe for some qt support)
+      #qt6-avif-image-plugin (Maybe for some qt support)
+      #qt6-base (Maybe for some qt support)
+      #qt6-declarative (Maybe for some qt support)
+      #qt6-imageformats (Maybe for some qt support)
+      #qt6-multimedia (Maybe for some qt support)
+      #qt6-positioning (Maybe for some qt support)
+      #qt6-quicktimeline (Maybe for some qt support)
+      #qt6-sensors (Maybe for some qt support)
+      #qt6-svg (Maybe for some qt support)
+      #qt6-tools (Maybe for some qt support)
+      #qt6-translations (Maybe for some qt support)
+      #qt6-virtualkeyboard (Maybe for some qt support)
+      #qt6-wayland (Maybe for some qt support)
+      kdePackages.syntax-highlighting #syntax-highlighting (Used in Quickshell config)
+      upower #upower (Used in Quickshell config)
+      wtype #wtype (Used in Hyprland scripts/fuzzel-emoji.sh)
+      ydotool #ydotool (Used in Quickshell config)
 
 
       ### illogical-impulse-widgets
-      #fuzzel
-      #glib2 # for `gsettings` it seems?
-      #imagemagick
-      #hypridle
-      #hyprutils
-      #hyprlock
-      #hyprpicker
-      #nm-connection-editor (TODO: Not needed?)
+      fuzzel #fuzzel (Used in Hyprland and Quickshell config; its config is also included)
+      glib #glib2 (Provide executable gsettings) (Used in install script, also in matugen and quickshell config)
+      imagemagick #imagemagick (Provide executable: magick) (Used in Quickshell config)
+      #hypridle (DUPLICATE)
+      #hyprutils (DUPLICATE)
+      #hyprlock (DUPLICATE)
+      #hyprpicker (DUPLICATE)
+      #nm-connection-editor (TODO: Not needed)
       songrec #songrec (Used in Quickshell config)
       translate-shell #translate-shell (Used in Quickshell config)
       wlogout #wlogout (Used in Hyprland config)
