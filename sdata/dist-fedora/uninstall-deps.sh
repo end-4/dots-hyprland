7016b599335bf894ea4a68350660c48078ea7cc7
Elysia <1391048988@qq.com>
2025-11-21 02:03:29 +0800

    fix: uninstall.sh is now working properly.

--- sdata/dist-fedora/uninstall-deps.sh
+++ sdata/dist-fedora/uninstall-deps.sh
@@ -1,10 +1,12 @@
 # This script is meant to be sourced.
 # It's not for directly running.
 
-user_config="${REPO_ROOT}/sdata/dist-fedora/user-config.yaml"
-yq -r '.dnf.transaction_ids[]? | reverse' "$user_config" | while read -r tx_id; do
+user_data="${REPO_ROOT}/sdata/dist-fedora/user_data.yaml"
+yq eval '.dnf.transaction_ids // [] | reverse[]' "$user_data" | while read -r tx_id; do
     echo -e "\n========================================"
-    echo "Rolling back DNF Transactions ID：$tx_id"
+    echo "Rolling back DNF Transactions ID: $tx_id"
     echo "========================================"
-    v sudo dnf history undo -y "$tx_id"
+    dnf history info "$tx_id"
+    echo -e "\nProceed to undo this transaction? "
+    v sudo dnf history undo -y "$tx_id" </dev/tty
 done
