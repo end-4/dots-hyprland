9cb5cc1416ca9d98118c464ffcc5c9536a9f4207
jwihardi <allarkvarkk@gmail.com>
2025-11-27 15:29:30 -0500

    added gentoo uninstall

---
+++ sdata/dist-gentoo/uninstall-deps.sh
@@ -0,0 +1,8 @@
+# This script is meant to be sourced.
+# It's not for directly running.
+
+for i in illogical-impulse-{quickshell-git,audio,backlight,basic,bibata-modern-classic-bin,fonts-themes,hyprland,kde,microtex-git,oneui4-icons-git,portal,python,screencapture,toolkit,widgets}; do
+  v sudo emerge --unmerge $i
+done
+
+v sudo emerge --depclean
