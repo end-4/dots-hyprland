f6cb8480b53a8297f1621228dd0a7d2b6dcda824
Thierry Miqueias Melo Madeira <171850673+masthierryi@users.noreply.github.com>
2025-12-07 13:22:01 +0000

    Update start_vlc.sh

--- dots-extra/other-files/start_vlc.sh
+++ dots-extra/other-files/start_vlc.sh
@@ -2,5 +2,7 @@
 
 # Inicia o VLC: -Z (Aleatório), -L (Loop), e desassocia do shell
 vlc -Z -L --no-playlist-autostart  /mnt/ssd2/music & disown
+sleep 0.1
+hyprctl dispatch togglespecialworkspace
 
 exit
