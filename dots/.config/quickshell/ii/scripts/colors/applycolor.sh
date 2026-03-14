e0f287514169c8cd001439a895a0868e3d5aa4e1
end-4 <97237370+end-4@users.noreply.github.com>
2026-03-14 22:04:57 +0100

    changes from main

--- dots/.config/quickshell/ii/scripts/colors/applycolor.sh
+++ dots/.config/quickshell/ii/scripts/colors/applycolor.sh
@@ -27,7 +27,22 @@ IFS=$'\n'
 colorlist=($colornames)     # Array of color names
 colorvalues=($colorstrings) # Array of color values
 
-apply_term() {
+apply_kitty() {  
+  # Check if terminal escape sequence template exists
+  if [ ! -f "$SCRIPT_DIR/terminal/kitty-theme.conf" ]; then
+    echo "Template file not found for Kitty theme. Skipping that."
+    return
+  fi
+  # Copy template
+  mkdir -p "$STATE_DIR"/user/generated/terminal
+  cp "$SCRIPT_DIR/terminal/kitty-theme.conf" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
+  # Apply colors
+  for i in "${!colorlist[@]}"; do
+    sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
+  done
+}
+
+apply_anyterm() {
   # Check if terminal escape sequence template exists
   if [ ! -f "$SCRIPT_DIR/terminal/sequences.txt" ]; then
     echo "Template file not found for Terminal. Skipping that."
@@ -50,6 +65,11 @@ apply_term() {
       } & disown || true
     fi
   done
+}
+
+apply_term() {
+  apply_kitty
+  apply_anyterm
 }
 
 apply_qt() {
