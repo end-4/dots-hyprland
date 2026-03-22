950356e47dab647836454b5c787b0352fe965113
end-4 <97237370+end-4@users.noreply.github.com>
2026-03-22 10:09:11 +0100

    anti-flashbang: adjust brightness curve

--- dots/.config/quickshell/ii/services/hyprlandAntiFlashbangShader/anti-flashbang.glsl
+++ dots/.config/quickshell/ii/services/hyprlandAntiFlashbangShader/anti-flashbang.glsl
@@ -10,9 +10,9 @@ float overlayOpacityForBrightness(float x) {
     
     // Will a fancy curve help?... I'll have to experiment more at night
     // float y = pow(x, 2.0) * 0.75;
-    // float y = (1.0 - exp(-x))*1.15;
+    float y = (1.0 - exp(-x))*1.19;
     // float y = (1.0 - exp(-pow((x-0.15), 0.6)))*1.18;
-    float y = x*0.75;
+    // float y = x*0.75;
 
     return min(max(y, 0.001), 1.0);
 }
