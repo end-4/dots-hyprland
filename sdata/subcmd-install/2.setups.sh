febc667442907f0dd9e6ac24b9eee6c48608d4ed
fb24m <hello@fb24m.ru>
2026-04-05 17:03:26 +0400

    hide window buttons

--- sdata/subcmd-install/2.setups.sh
+++ sdata/subcmd-install/2.setups.sh
@@ -68,5 +68,6 @@ if [[ "$OS_GROUP_ID" == "gentoo" ]]; then
 fi
 
 v gsettings set org.gnome.desktop.interface font-name 'Google Sans Flex Medium 11 @opsz=11,wght=500'
+v gsettings set org.gnome.desktop.wm.preferences button-layout ":"
 v gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 v kwriteconfig6 --file kdeglobals --group KDE --key widgetStyle Darkly
