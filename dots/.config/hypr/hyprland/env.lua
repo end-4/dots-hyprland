340811e0f74d57643fd3e9795ed61eee102da7eb
imitoy <imitoy@imitoy.top>
2026-05-13 16:03:09 +0800

    Fix HOME env in lua

--- dots/.config/hypr/hyprland/env.lua
+++ dots/.config/hypr/hyprland/env.lua
@@ -1,8 +1,10 @@
+local home_dir = os.getenv("HOME")
+
 -- Wayland
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
 -- Applications
-hl.env("XDG_DATA_DIRS", "$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share")
+hl.env("XDG_DATA_DIRS", home_dir .. "/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share")
 
 -- Themes
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
@@ -10,4 +12,4 @@ hl.env("QT_QPA_PLATFORMTHEME", "kde")
 hl.env("XDG_MENU_PREFIX", "plasma-")
 
 -- Virtual environment
-hl.env("ILLOGICAL_IMPULSE_VIRTUAL_ENV", "$HOME/.local/state/quickshell/.venv")
+hl.env("ILLOGICAL_IMPULSE_VIRTUAL_ENV", home_dir .. "/.local/state/quickshell/.venv")
