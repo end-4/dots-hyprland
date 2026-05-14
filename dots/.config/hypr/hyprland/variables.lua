f0f5ed16fb7ec933e9cf5992dbf8b78038aa6f9d
end-4 <97237370+end-4@users.noreply.github.com>
2026-05-14 08:16:47 +0200

    hl: keybinds: fix workspace groups, categorize descriptions

--- dots/.config/hypr/hyprland/variables.lua
+++ dots/.config/hypr/hyprland/variables.lua
@@ -15,3 +15,5 @@ textEditor = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'kate' '
 volumeMixer = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'pavucontrol-qt' 'pavucontrol'"
 settingsApp = "XDG_CURRENT_DESKTOP=gnome ~/.config/hypr/hyprland/scripts/launch_first_available.sh 'qs -p ~/.config/quickshell/$qsConfig/settings.qml' 'systemsettings' 'gnome-control-center' 'better-control'"
 taskManager = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'gnome-system-monitor' 'plasma-systemmonitor --page-name Processes' 'command -v btop && kitty -1 fish -c btop'"
+
+workspaceGroupSize = 10
