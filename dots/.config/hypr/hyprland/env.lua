7af6e9adf939505097a5fc1180c2d8657482bb76
GregorVal <106101599+GregorVal@users.noreply.github.com>
2026-05-17 14:46:26 +0200

    Fix: XDG_DATA_DIRS now expands correctly
    Previously the environmental variable would become literally
    *:$XDG_DATA_DIRS
    Now the variable is expanded correctly

--- dots/.config/hypr/hyprland/env.lua
+++ dots/.config/hypr/hyprland/env.lua
@@ -4,9 +4,12 @@ local home_dir = os.getenv("HOME")
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
 -- Applications
+local xdg_data_dirs_old = os.getenv("XDG_DATA_DIRS") or ""
 hl.env(
 	"XDG_DATA_DIRS",
-	"$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:$XDG_DATA_DIRS"
+	home_dir
+		.. "/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:"
+		.. xdg_data_dirs_old
 )
 
 -- Themes
