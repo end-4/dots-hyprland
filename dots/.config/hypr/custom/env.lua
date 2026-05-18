2200fd4d75d183082153d2f10edf45e624d6b636
Jott55 <josuhbs@gmail.com>
2026-05-18 17:08:44 -0300

    fix environments by adding a parser

--- dots/.config/hypr/custom/env.lua
+++ dots/.config/hypr/custom/env.lua
@@ -1,1 +1,22 @@
+local function parse_envs(str)
+    local envs = ""
+    local rstr = str
+    for w in str:gmatch("$[%w_]+") do
+        if not string.match(envs, w) then
+            envs = w .. " " .. envs
+        end
+    end
+    for w in envs:gmatch("[%w_]+") do
+        local env = os.getenv(w)
+        if env then
+            rstr = string.gsub(rstr, "$"..w, env)
+        else
+            rstr = string.gsub(rstr,"$".. w, "")
+        end
+    end
+    return rstr
+end
 
+
+-- Example
+-- hl.env("ANDROID_HOME", parse_envs("$HOME/Android/Sdk"))
--- dots/.config/hypr/hyprland/env.lua
+++ dots/.config/hypr/hyprland/env.lua
@@ -1,10 +1,27 @@
-local home_dir = os.getenv("HOME")
+local function parse_envs(str)
+    local envs = ""
+    local rstr = str
+    for w in str:gmatch("$[%w_]+") do
+        if not string.match(envs, w) then
+            envs = w .. " " .. envs
+        end
+    end
+    for w in envs:gmatch("[%w_]+") do
+        local env = os.getenv(w)
+        if env then
+            rstr = string.gsub(rstr, "$"..w, env)
+        else
+            rstr = string.gsub(rstr,"$".. w, "")
+        end
+    end
+    return rstr
+end
 
 -- Wayland
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
 -- Applications
-hl.env("XDG_DATA_DIRS", home_dir .. "/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:$XDG_DATA_DIRS")
+hl.env("XDG_DATA_DIRS", parse_envs("$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:$XDG_DATA_DIRS"))
 
 -- Themes
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
@@ -12,4 +29,4 @@ hl.env("QT_QPA_PLATFORMTHEME", "kde")
 hl.env("XDG_MENU_PREFIX", "plasma-")
 
 -- Virtual environment
-hl.env("ILLOGICAL_IMPULSE_VIRTUAL_ENV", home_dir .. "/.local/state/quickshell/.venv")
+hl.env("ILLOGICAL_IMPULSE_VIRTUAL_ENV", parse_envs("$HOME/.local/state/quickshell/.venv"))
