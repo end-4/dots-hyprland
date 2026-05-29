2f8814e3ce3fa1ad382c10e32a444655713a0bc8
Leo Aqua <contact@leoaqua.de>
2026-05-29 22:50:54 +0200

    add openRGB integration

--- dots/.config/quickshell/ii/scripts/colors/applycolor.sh
+++ dots/.config/quickshell/ii/scripts/colors/applycolor.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 term_alpha=100 #Set this to < 100 make all your terminals transparent
 # sleep 0 # idk i wanted some delay or colors dont get applied properly
 if [ ! -d "$STATE_DIR"/user/generated ]; then
-  mkdir -p "$STATE_DIR"/user/generated
+    mkdir -p "$STATE_DIR"/user/generated
 fi
 cd "$CONFIG_DIR" || exit
 
@@ -27,67 +27,71 @@ IFS=$'\n'
 colorlist=($colornames)     # Array of color names
 colorvalues=($colorstrings) # Array of color values
 
-apply_kitty() {  
-  # Check if terminal escape sequence template exists
-  if [ ! -f "$SCRIPT_DIR/terminal/kitty-theme.conf" ]; then
-    echo "Template file not found for Kitty theme. Skipping that."
-    return
-  fi
-  # Copy template
-  mkdir -p "$STATE_DIR"/user/generated/terminal
-  cp "$SCRIPT_DIR/terminal/kitty-theme.conf" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
-  # Apply colors
-  for i in "${!colorlist[@]}"; do
-    sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
-  done
-
-  # Reload
-  if ! pgrep -f kitty >/dev/null; then
-    return
-  fi
-  kill -SIGUSR1 $(pidof kitty)
+apply_kitty() {
+    # Check if terminal escape sequence template exists
+    if [ ! -f "$SCRIPT_DIR/terminal/kitty-theme.conf" ]; then
+        echo "Template file not found for Kitty theme. Skipping that."
+        return
+    fi
+    # Copy template
+    mkdir -p "$STATE_DIR"/user/generated/terminal
+    cp "$SCRIPT_DIR/terminal/kitty-theme.conf" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
+    # Apply colors
+    for i in "${!colorlist[@]}"; do
+        sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
+    done
+    
+    # Reload
+    if ! pgrep -f kitty >/dev/null; then
+        return
+    fi
+    kill -SIGUSR1 $(pidof kitty)
 }
 
 apply_anyterm() {
-  # Check if terminal escape sequence template exists
-  if [ ! -f "$SCRIPT_DIR/terminal/sequences.txt" ]; then
-    echo "Template file not found for Terminal. Skipping that."
-    return
-  fi
-  # Copy template
-  mkdir -p "$STATE_DIR"/user/generated/terminal
-  cp "$SCRIPT_DIR/terminal/sequences.txt" "$STATE_DIR"/user/generated/terminal/sequences.txt
-  # Apply colors
-  for i in "${!colorlist[@]}"; do
-    sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/sequences.txt
-  done
-
-  sed -i "s/\$alpha/$term_alpha/g" "$STATE_DIR/user/generated/terminal/sequences.txt"
-
-  for file in /dev/pts/*; do
-    if [[ $file =~ ^/dev/pts/[0-9]+$ ]]; then
-      {
-      cat "$STATE_DIR"/user/generated/terminal/sequences.txt >"$file"
-      } & disown || true
+    # Check if terminal escape sequence template exists
+    if [ ! -f "$SCRIPT_DIR/terminal/sequences.txt" ]; then
+        echo "Template file not found for Terminal. Skipping that."
+        return
     fi
-  done
+    # Copy template
+    mkdir -p "$STATE_DIR"/user/generated/terminal
+    cp "$SCRIPT_DIR/terminal/sequences.txt" "$STATE_DIR"/user/generated/terminal/sequences.txt
+    # Apply colors
+    for i in "${!colorlist[@]}"; do
+        sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/sequences.txt
+    done
+    
+    sed -i "s/\$alpha/$term_alpha/g" "$STATE_DIR/user/generated/terminal/sequences.txt"
+    
+    for file in /dev/pts/*; do
+        if [[ $file =~ ^/dev/pts/[0-9]+$ ]]; then
+            {
+                cat "$STATE_DIR"/user/generated/terminal/sequences.txt >"$file"
+            } & disown || true
+        fi
+    done
 }
 
 apply_term() {
-  apply_anyterm &
-  apply_kitty &
+    apply_anyterm &
+    apply_kitty &
+}
+
+apply_openrgb() {
+    python "$CONFIG_DIR/scripts/colors/openRGB/apply_openrgb.py"
 }
 
 # Check if terminal theming is enabled in config
 CONFIG_FILE="$XDG_CONFIG_HOME/illogical-impulse/config.json"
 if [ -f "$CONFIG_FILE" ]; then
-  enable_terminal=$(jq -r '.appearance.wallpaperTheming.enableTerminal' "$CONFIG_FILE")
-  if [ "$enable_terminal" = "true" ]; then
-    apply_term &
-  fi
+    enable_terminal=$(jq -r '.appearance.wallpaperTheming.enableTerminal' "$CONFIG_FILE")
+    if [ "$enable_terminal" = "true" ]; then
+        apply_term &
+    fi
 else
-  echo "Config file not found at $CONFIG_FILE. Applying terminal theming by default."
-  apply_term &
+    echo "Config file not found at $CONFIG_FILE. Applying terminal theming by default."
+    apply_term &
 fi
 
 # apply_qt & # Qt theming is already handled by kde-material-colors
---
+++ dots/.config/quickshell/ii/scripts/colors/openRGB/apply_openrgb.py
@@ -0,0 +1,77 @@
+from openrgb import OpenRGBClient
+from openrgb.utils import RGBColor
+from scipy.interpolate import interp1d
+import os
+from time import sleep
+import argparse
+
+parser = argparse.ArgumentParser(description="Apply color on OpenRGB devices with a smooth transition")
+parser.add_argument(
+    "--duration",
+    "-d",
+    type=float,
+    default=0.5,
+    help="Duraton of color swap animation",
+)
+parser.add_argument(
+    "--interpolation-steps",
+    "-i",
+    type=int,
+    default=100,
+    help="Number of steps to swap the colors (lower=choppyer, higher=smoother)",
+)
+parser.add_argument(
+    "--color",
+    "-c",
+    type=str,
+    help="HEX color to transition to",
+
+)
+args = parser.parse_args()
+
+def hexToRGB(hexColor) -> list[int]:
+    hexColor = hexColor.removeprefix("#")
+    hexColor = [hexColor[i : i + 2] for i in range(0, 6, 2)]  # Split hex values
+    intColor = [int(hexValue, 16) for hexValue in hexColor]  # Convert to int
+    return intColor
+
+
+TRANSITION_DURATION = args.duration
+INTERPOLATION_STEPS = args.interpolation_steps
+
+xdg_state_home = os.environ.get("XDG_STATE_HOME", os.path.expanduser("~/.local/state"))
+state_dir = os.path.join(xdg_state_home, "quickshell")
+
+
+client = OpenRGBClient(name="quickshell")
+old_color = [
+    client.devices[0].leds[0].colors[0].red,
+    client.devices[0].leds[0].colors[0].green,
+    client.devices[0].leds[0].colors[0].blue,
+]  # A bit naive but should do the trick
+
+with open(state_dir + "/user/generated/color.txt", "r") as f:
+    new_color = hexToRGB(f.read())
+
+
+if args.color != None:
+    new_color = hexToRGB(args.color)
+
+
+for dev in client.devices:
+
+    # set all device modes to 'Direct' (0)
+    if dev.active_mode != 0:
+        dev.set_mode(mode=0, save=True)
+
+
+y_known = [old_color, new_color]
+
+f = interp1d([0, 1], y_known, axis=0)
+
+
+for i in range(INTERPOLATION_STEPS):
+    t = i / (INTERPOLATION_STEPS - 1)
+    interp_color = [int(i) for i in f(t)]
+    client.set_color(RGBColor(*interp_color), True)
+    sleep(TRANSITION_DURATION/INTERPOLATION_STEPS)
